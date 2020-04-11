Y4CXP543/violentmonkey

commit 072b773914dfdf811fc1b97553189c5733f90c4e

feat: auto-refresh values in editor + accessibility
* navigate in value list via Tab
* Ctrl-Del to delete the active value
* Alt-PageUp/Down to switch code-settings-values-help
* fix: column-span ignores tables per new spec

src/options/views/edit/index.vue
@@ -3,29 +3,11 @@
     <div class="edit-header flex">
       <nav>
         <div
-          class="edit-nav-item"
-          :class="{active: nav === 'code'}"
-          v-text="i18n('editNavCode')"
-          @click="nav = 'code'"
+          v-for="(label, navKey) in navItems" :key="navKey"
+          class="edit-nav-item" :class="{active: nav === navKey}"
+          v-text="label"
+          @click="nav = navKey"
         />
-        <div
-          class="edit-nav-item"
-          :class="{active: nav === 'settings'}"
-          v-text="i18n('editNavSettings')"
-          @click="nav = 'settings'"
-        />
-        <div
-          class="edit-nav-item"
-          :class="{active: nav === 'values'}"
-          v-if="scriptId"
-          v-text="i18n('editNavValues')"
-          @click="nav = 'values'"
-        />
-        <div
-          class="edit-nav-item"
-          :class="{active: nav === 'help'}"
-          @click="nav = 'help'"
-        >?</div>
       </nav>
       <div class="edit-name text-center ellipsis flex-1 mr-1" v-text="scriptName"/>
       <div class="edit-hint text-right ellipsis mr-1">
@@ -42,18 +24,31 @@
     </div>
     <div class="frame-block flex-auto pos-rel">
       <vm-code
-        v-show="nav === 'code'" class="abs-full" ref="code" :editing="nav === 'code'"
-        v-model="code" :commands="commands"
+        class="abs-full"
+        v-model="code"
+        ref="code"
+        v-show="nav === 'code'"
+        :active="nav === 'code'"
+        :commands="commands"
       />
       <vm-settings
-        v-show="nav === 'settings'" class="abs-full edit-body"
-        :value="script" :settings="settings"
+        class="abs-full edit-body"
+        v-show="nav === 'settings'"
+        :active="nav === 'settings'"
+        :settings="settings"
+        :value="script"
       />
       <vm-values
-        :show="nav === 'values'" class="abs-full edit-body" :script="script"
+        class="abs-full edit-body"
+        v-show="nav === 'values'"
+        :active="nav === 'values'"
+        :script="script"
       />
       <vm-help
-        v-if="nav === 'help'" class="abs-full edit-body"
+        class="abs-full edit-body"
+        v-show="nav === 'help'"
+        :active="nav === 'help'"
+        :navLabels="Object.values(navItems)"
         :target="this.$refs.code"
       />
     </div>
@@ -106,14 +101,14 @@ export default {
   },
   data() {
     return {
-      nav: 'code',
+      nav: '',
       canSave: false,
       script: null,
       code: '',
       settings: {},
       commands: {
         save: this.save,
-        close: () => this.close({ fromCM: true }),
+        close: this.close,
         showHelp: () => {
           this.nav = 'help';
         },
@@ -121,6 +116,12 @@ export default {
     };
   },
   computed: {
+    navItems: () => ({
+      code: i18n('editNavCode'),
+      settings: i18n('editNavSettings'),
+      values: i18n('editNavValues'),
+      help: '?',
+    }),
     scriptName() {
       const { custom, meta } = this.script || {};
       const scriptName = custom && custom.name || meta && meta.name;
@@ -141,14 +142,13 @@ export default {
         showMessage({ text: `${this.initial.message}\n\n${error}` });
       }
     },
-    nav() {
-      setTimeout(() => this.nav === 'code' && this.setFocusToCode());
-    },
   },
   created() {
     this.script = this.initial;
+    document.addEventListener('keydown', this.switchPanel);
   },
   async mounted() {
+    this.nav = 'code';
     const id = this.script?.props?.id;
     if (id) {
       this.code = await sendCmd('GetScriptCode', id);
@@ -207,11 +207,7 @@ export default {
         showMessage({ text: err });
       }
     },
-    async close({ fromCM } = {}) {
-      if (fromCM && this.nav !== 'code') {
-        this.nav = 'code';
-        return;
-      }
+    async close() {
       try {
         if (this.canSave) {
           showingConfirmation = true;
@@ -228,8 +224,18 @@ export default {
     saveClose() {
       this.save().then(this.close);
     },
-    setFocusToCode() {
-      this.$refs.code.cm.focus();
+    switchPanel(e) {
+      if (!e.ctrlKey && !e.metaKey && !e.shiftKey) {
+        if (e.altKey) {
+          const dir = e.code === 'PageDown' && 1 || e.code === 'PageUp' && -1;
+          if (dir) {
+            const keys = Object.keys(this.navItems);
+            this.nav = keys[(keys.indexOf(this.nav) + dir + keys.length) % keys.length];
+          }
+        } else if (e.code === 'Escape') {
+          this.nav = 'code';
+        }
+      }
     },
     toggleUnloadSentry(state) {
       const onOff = `${state ? 'add' : 'remove'}EventListener`;
@@ -254,6 +260,7 @@ export default {
   beforeDestroy() {
     store.title = null;
     this.toggleUnloadSentry(false);
+    document.removeEventListener('keydown', this.switchPanel);
   },
 };
 </script>

src/options/views/edit/values.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="edit-values" v-show="show">
+  <div class="edit-values" ref="container">
     <div class="mb-1">
       <button @click="onNew">+</button>
       <div class="inline-block ml-2" v-if="totalPages > 1">
@@ -12,21 +12,23 @@
       <div class="edit-values-empty" v-if="!keys.length">
         <div v-text="i18n('noValues')"></div>
       </div>
-      <div
+      <a
         v-for="key in currentPage.data"
         :key="key"
         class="edit-values-row flex"
-        @click="onEdit(key)">
+        href="#"
+        @keydown.delete.ctrl.exact="onRemove(key)"
+        @click.prevent="onEdit(key)">
         <div class="ellipsis">
           <span v-text="key"></span>
-          <div class="edit-values-btn">
-            <span @click.stop="onRemove(key)">
-              <icon name="trash" />
-            </span>
+          <div class="edit-values-btn" @click.stop="onRemove(key)">
+            <tooltip :content="`Ctrl-Del: ${i18n('buttonRemove')}`">
+              <icon name="trash"/>
+            </tooltip>
           </div>
         </div>
         <div class="ellipsis flex-auto" v-text="getValue(key, true)"></div>
-      </div>
+      </a>
     </div>
     <div class="edit-values-panel flex flex-col" v-if="current">
       <div class="flex mb-1">
@@ -55,13 +57,17 @@
 </template>
 
 <script>
+import Tooltip from 'vueleton/lib/tooltip/bundle';
 import { dumpScriptValue, sendCmd } from '#/common';
 import Icon from '#/common/ui/icon';
+import storage from '#/common/storage';
 import { showMessage } from '../../utils';
 
 const PAGE_SIZE = 25;
 const MAX_LENGTH = 1024;
 const MAX_JSON_DURATION = 10; // ms
+let scriptStorageKey;
+let focusedElement;
 
 const reparseJson = (str) => {
   try {
@@ -73,9 +79,10 @@ const reparseJson = (str) => {
 };
 
 export default {
-  props: ['show', 'script'],
+  props: ['active', 'script'],
   components: {
     Icon,
+    Tooltip,
   },
   data() {
     return {
@@ -105,16 +112,24 @@ export default {
     },
   },
   watch: {
-    show(show) {
-      if (show && !this.keys) this.refresh();
-    },
-    current(val) {
+    active(val) {
       if (val) {
+        storage.value.getOne(this.script.props.id).then(this.setData);
+        scriptStorageKey = storage.value.prefix + this.script?.props.id;
+        (focusedElement || this.$refs.container.querySelector('button')).focus();
+      }
+      browser.storage.onChanged[`${val ? 'add' : 'remove'}Listener`](this.onStorageChanged);
+    },
+    current(val, oldVal) {
+      if (val) {
+        focusedElement = document.activeElement;
         this.$nextTick(() => {
           const el = this.$refs[val.isNew ? 'key' : 'value'];
           el.setSelectionRange(0, 0);
           el.focus();
         });
+      } else if (oldVal) {
+        focusedElement?.focus();
       }
     },
   },
@@ -130,13 +145,10 @@ export default {
       }
       return value;
     },
-    refresh() {
-      sendCmd('GetValueStore', this.script.props.id)
-      .then((values) => {
-        this.values = values;
-        this.keys = Object.keys(values).sort();
-        this.page = 1;
-      });
+    setData(values = {}) {
+      this.values = values;
+      this.keys = Object.keys(values).sort();
+      this.page = Math.min(this.page, Math.ceil(this.keys.length / PAGE_SIZE)) || 1;
     },
     updateValue({ key, isNew }) {
       const rawValue = dumpScriptValue(this.current?.jsonValue) || '';
@@ -208,17 +220,25 @@ export default {
       }
       current.jsonPaused = performance.now() - t0 > MAX_JSON_DURATION;
     },
-  },
-  created() {
-    let unwatch;
-    const init = () => {
-      if (this.show) {
-        this.refresh();
-        if (unwatch) unwatch();
+    onStorageChanged(changes) {
+      const data = changes[scriptStorageKey]?.newValue;
+      if (data) {
+        this.setData(data);
+        const { current } = this;
+        if (current) {
+          const newText = this.getValue(current.key);
+          if (newText === current.value) {
+            current.isNew = false;
+          } else {
+            // Keeping the same this.current to avoid triggering `watch` observer
+            Object.keys(current)
+            .filter(k => k !== 'key' && k !== 'value')
+            .forEach(k => delete current[k]);
+            current.value = newText;
+          }
+        }
       }
-    };
-    unwatch = this.$watch('show', init);
-    init();
+    },
   },
 };
 </script>
@@ -227,6 +247,8 @@ export default {
 .edit-values {
   &-row {
     border: 1px solid var(--fill-2);
+    color: unset;
+    text-decoration: none;
     &:not(:first-child) {
       border-top: 0;
     }
@@ -242,8 +264,16 @@ export default {
         border-left: 1px solid var(--fill-2);
       }
     }
-    :not(:hover) .edit-values-btn {
-      display: none;
+    &:focus,
+    &:hover {
+      background-color: var(--fill-0-5);
+    }
+    &:focus {
+      text-decoration: underline;
+    }
+    &:focus .edit-values-btn,
+    &:hover .edit-values-btn {
+      display: block;
     }
   }
   &-empty {
@@ -276,8 +306,9 @@ export default {
     top: 0;
     right: 0;
     padding: 4px;
-    background: var(--bg);
-    box-shadow: -5px 0 5px var(--bg);
+    background: inherit;
+    box-shadow: 0 0 5px 5px var(--fill-0-5);
+    display: none;
   }
 }
 </style>

src/common/browser.js
@@ -90,6 +90,7 @@ if (!global.browser?.runtime?.sendMessage) {
         set: wrapAsync,
         remove: wrapAsync,
       },
+      onChanged: true,
     },
     tabs: {
       onCreated: true,

src/common/ui/code.vue
@@ -179,6 +179,7 @@ function replaceAll(cm, state) {
 
 export default {
   props: {
+    active: Boolean,
     readonly: {
       type: Boolean,
       default: false,
@@ -191,11 +192,6 @@ export default {
       type: Object,
       default: null,
     },
-    global: {
-      type: Boolean,
-      default: true,
-    },
-    editing: Boolean,
   },
   components: {
     Tooltip,
@@ -217,6 +213,16 @@ export default {
     };
   },
   watch: {
+    active(val) {
+      const onOff = val ? 'on' : 'off';
+      this.cm[onOff]('blur', this.onKeyDownToggler);
+      this.cm[onOff]('focus', this.onKeyDownToggler);
+      if (val) {
+        this.cm?.focus();
+      } else {
+        window.removeEventListener('keydown', this.onKeyDown);
+      }
+    },
     value(value) {
       if (value === this.cached) return;
       this.cached = value;
@@ -318,12 +324,19 @@ export default {
       });
       this.$emit('ready', cm);
     },
+    /* reroute hotkeys back to CM when it isn't focused,
+       but ignore `window` blur (`evt` param is absent) */
+    onKeyDownToggler(cm, evt) {
+      if (evt) {
+        window[`${evt.type === 'blur' ? 'add' : 'remove'}EventListener`]('keydown', this.onKeyDown);
+      }
+    },
     onKeyDown(e) {
       const name = CodeMirror.keyName(e);
       if (!this.cm) return;
       [
         this.cm.options.extraKeys,
-        this.editing && this.cm.options.keyMap,
+        this.cm.options.keyMap,
       ].some(keyMap => keyMap && this.lookupKey(name, keyMap, e) === 'handled');
     },
     lookupKey(name, keyMap, e) {
@@ -416,16 +429,7 @@ export default {
     // pressing Tab key inside a line with no selection will reuse indent size
     if (!opts.tabSize) this.cm.options.tabSize = this.cm.options.indentUnit;
     this.debouncedFind = debounce(this.searchInPlace, 100);
-    if (this.global) {
-      // reroute a hotkey only when CM isn't focused and thus can't handle it
-      this.cm.on('blur', () => window.addEventListener('keydown', this.onKeyDown));
-      this.cm.on('focus', () => window.removeEventListener('keydown', this.onKeyDown));
-    }
     this.$refs.code.addEventListener('copy', this.onCopy);
-  },
-  beforeDestroy() {
-    if (this.global) window.removeEventListener('keydown', this.onKeyDown, false);
-    this.$refs.code?.removeEventListener('copy', this.onCopy);
   },
 };
 </script>

src/options/views/edit/help.vue
@@ -7,12 +7,10 @@
     </div>
     <div class="keyboard">
       <h3 v-text="i18n('editHelpKeyboard')"/>
-      <table>
-        <tr v-for="([key, cmd]) of hotkeys" :key="key">
-          <th class="monospace-font">{{key}}</th>
-          <td>{{cmd}}</td>
-        </tr>
-      </table>
+      <dl v-for="([key, cmd]) of hotkeys" :key="key">
+        <dt class="monospace-font" v-text="key"></dt>
+        <dd v-text="cmd"></dd>
+      </dl>
     </div>
   </div>
 </template>
@@ -22,16 +20,24 @@ import CodeMirror from 'codemirror';
 import { forEachEntry } from '#/common/object';
 
 export default {
-  props: ['target'],
+  props: ['active', 'navLabels', 'target'],
   data() {
     return {
       hotkeys: null,
     };
   },
-  mounted() {
-    const cmOpts = this.target.cm.options;
-    this.hotkeys = Object.entries(expandKeyMap({}, cmOpts.extraKeys, cmOpts.keyMap))
-    .sort((a, b) => compareString(a, b, 1) || compareString(a, b, 0));
+  watch: {
+    active(val) {
+      if (val && !this.hotkeys) {
+        const cmOpts = this.target.cm.options;
+        this.hotkeys = [
+          ['Alt-PageUp', ` ${this.navLabels.join(' < ')}`],
+          ['Alt-PageDown', ` ${this.navLabels.join(' > ')}`],
+          ...Object.entries(expandKeyMap({}, cmOpts.extraKeys, cmOpts.keyMap))
+          .sort((a, b) => compareString(a, b, 1) || compareString(a, b, 0)),
+        ];
+      }
+    },
   },
 };
 
@@ -63,13 +69,20 @@ function expandKeyMap(res, ...maps) {
     margin: .5em 0;
   }
   .keyboard {
-    column-width: 20em;
+    column-width: 25em;
     h3 {
       column-span: all;
     }
-    th {
+    dl {
+      display: flex;
+      align-items: center;
+      padding: .25em 0;
+    }
+    dt {
       text-align: right;
       padding-right: .5em;
+      flex: 0 40%;
+      font-weight: bold;
     }
   }
 }

src/options/views/edit/settings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="edit-settings">
+  <div class="edit-settings" ref="container">
     <h4 v-text="i18n('editLabelSettings')"></h4>
     <div class="form-group">
       <label>
@@ -100,7 +100,7 @@ import { i18n } from '#/common';
 import { objectGet } from '#/common/object';
 
 export default {
-  props: ['value', 'settings'],
+  props: ['active', 'settings', 'value'],
   components: {
     Tooltip,
   },
@@ -119,6 +119,13 @@ export default {
         updateURL: objectGet(value, 'meta.updateURL') || i18n('hintUseDownloadURL'),
         downloadURL: objectGet(value, 'meta.downloadURL') || objectGet(value, 'custom.lastInstallURL'),
       };
+    },
+  },
+  watch: {
+    active(val) {
+      if (val) {
+        this.$refs.container.querySelector('input').focus();
+      }
     },
   },
 };
@@ -147,6 +154,9 @@ export default {
     > textarea {
       min-height: 5em;
     }
+  }
+  label:focus-within span {
+    text-decoration: underline;
   }
 }
 </style>

commit a87fc66dc62cc48b5817eee9e4c2d66fbc3cf641

fix the values editor (#958)
* fix: values page navigation

* fix: removing in value editor

* fix: hide Values tab in editor for new scripts (first implemented in d8bab6bf, broken in 072b7739)

src/options/views/edit/index.vue
@@ -116,20 +116,19 @@ export default {
     };
   },
   computed: {
-    navItems: () => ({
-      code: i18n('editNavCode'),
-      settings: i18n('editNavSettings'),
-      values: i18n('editNavValues'),
-      help: '?',
-    }),
+    navItems() {
+      return {
+        code: i18n('editNavCode'),
+        settings: i18n('editNavSettings'),
+        ...this.script?.props?.id && { values: i18n('editNavValues') },
+        help: '?',
+      };
+    },
     scriptName() {
       const { custom, meta } = this.script || {};
       const scriptName = custom && custom.name || meta && meta.name;
       store.title = scriptName;
       return scriptName;
-    },
-    scriptId() {
-      return this.script?.props?.id;
     },
   },
   watch: {

src/options/views/edit/values.vue
@@ -3,9 +3,9 @@
     <div class="mb-1">
       <button @click="onNew">+</button>
       <div class="inline-block ml-2" v-if="totalPages > 1">
-        <button :disabled="!hasPrevious" @click="page = currentPage.page - 1">&larr;</button>
-        <span class="mx-1" v-text="page"></span>
-        <button :disabled="!hasNext" @click="page = currentPage.page + 1">&rarr;</button>
+        <button :disabled="page === 1" @click="page -= 1">&larr;</button>
+        <span class="ml-1" v-text="page"/> / <span class="mr-1" v-text="totalPages"/>
+        <button :disabled="page >= totalPages" @click="page += 1">&rarr;</button>
       </div>
     </div>
     <div class="edit-values-table" v-if="keys">
@@ -13,7 +13,7 @@
         <div v-text="i18n('noValues')"></div>
       </div>
       <a
-        v-for="key in currentPage.data"
+        v-for="key in pageKeys"
         :key="key"
         class="edit-values-row flex"
         href="#"
@@ -21,7 +21,7 @@
         @click.prevent="onEdit(key)">
         <div class="ellipsis">
           <span v-text="key"></span>
-          <div class="edit-values-btn" @click.stop="onRemove(key)">
+          <div class="edit-values-btn" @click.stop.prevent="onRemove(key)">
             <tooltip :content="`Ctrl-Del: ${i18n('buttonRemove')}`">
               <icon name="trash"/>
             </tooltip>
@@ -88,27 +88,17 @@ export default {
     return {
       current: null,
       keys: null,
+      page: null,
       values: null,
     };
   },
   computed: {
     totalPages() {
-      if (!this.keys) return 0;
-      return Math.floor(this.keys.length / PAGE_SIZE) + 1;
+      return Math.ceil(this.keys?.length / PAGE_SIZE) || 0;
     },
-    currentPage() {
-      const page = Math.max(1, Math.min(this.page, this.totalPages));
-      const offset = PAGE_SIZE * (page - 1);
-      return {
-        page,
-        data: this.keys ? this.keys.slice(offset, offset + PAGE_SIZE) : null,
-      };
-    },
-    hasPrevious() {
-      return this.currentPage.page > 1;
-    },
-    hasNext() {
-      return this.currentPage.page < this.totalPages;
+    pageKeys() {
+      const offset = PAGE_SIZE * (this.page - 1);
+      return this.keys?.slice(offset, offset + PAGE_SIZE);
     },
   },
   watch: {
@@ -148,10 +138,10 @@ export default {
     setData(values = {}) {
       this.values = values;
       this.keys = Object.keys(values).sort();
-      this.page = Math.min(this.page, Math.ceil(this.keys.length / PAGE_SIZE)) || 1;
+      this.page = Math.min(this.page, this.totalPages) || 1;
     },
-    updateValue({ key, isNew }) {
-      const rawValue = dumpScriptValue(this.current?.jsonValue) || '';
+    updateValue({ key, jsonValue, isNew }) {
+      const rawValue = dumpScriptValue(jsonValue) || '';
       const { id } = this.script.props;
       return sendCmd('UpdateValue', { id, key, value: rawValue })
       .then(() => {