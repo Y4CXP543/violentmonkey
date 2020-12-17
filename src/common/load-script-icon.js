Y4CXP543/violentmonkey

commit 9b9a9835474a930540eea6515ffdbd22204223f7

fix: show SVG icons again

src/common/load-script-icon.js
@@ -32,12 +32,11 @@ export function loadScriptIcon(script, {
   return promise;
 }
 
-async function fetchImage(url) {
-  try {
-    const blob = await (await fetch(url)).blob();
-    await createImageBitmap(blob);
-    return true;
-  } catch (e) {
-    return false;
-  }
+function fetchImage(url) {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.src = url;
+    img.onload = () => resolve(true);
+    img.onerror = () => resolve(false);
+  });
 }

commit 587f0a5a761c28908ec47aea1751672c55f4eb75

fix: don't delay the popup while checking icons
regressed in 9b9a9835

src/common/load-script-icon.js
@@ -32,7 +32,17 @@ export function loadScriptIcon(script, {
   return promise;
 }
 
-function fetchImage(url) {
+async function fetchImage(url) {
+  /* The benefit of fetch+createImageBitmap is that it doesn't delay the popup in Chrome,
+     but it doesn't work with SVG so we're using DOM Image in case the modern method fails
+     or when the URL obviously contains an SVG */
+  if (!/^data:image\/svg|\.svgz?([#?].*)?$/.test(url)) {
+    try {
+      const blob = await (await fetch(url)).blob();
+      await createImageBitmap(blob);
+      return true;
+    } catch (e) { /* NOP */ }
+  }
   return new Promise((resolve) => {
     const img = new Image();
     img.src = url;