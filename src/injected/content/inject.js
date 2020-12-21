Y4CXP543/violentmonkey

commit 136c180421773c02069b3f1e45d7ca6ca9af5955

feat: implement document-body for content mode
* run document-idle content mode scripts after page mode scripts
* run setTimeout before each document-idle script
* run setTimeout in background context for reliability

src/injected/content/inject.js
@@ -6,10 +6,8 @@ import {
   browser,
 } from '#/common/consts';
 import { sendCmd } from '#/common';
-import { defineProperty, describeProperty, forEachEntry, objectPick } from '#/common/object';
-
+import { defineProperty, forEachKey, objectPick } from '#/common/object';
 import {
-  forEach, push,
   append, createElementNS, remove, NS_HTML,
   addEventListener, removeEventListener,
   log,
@@ -22,25 +20,25 @@ const VMInitInjection = window[process.env.INIT_FUNC_NAME];
 // (the prop is undeletable so a userscript can't fool us on reinjection)
 defineProperty(window, process.env.INIT_FUNC_NAME, { value: 1 });
 
-const { document, setTimeout } = global;
-// Userscripts in content mode may redefine head and documentElement
-const { get: getHead } = describeProperty(Document.prototype, 'head')
-  || describeProperty(HTMLDocument.prototype, 'head'); // old FF before 61;
-const { get: getDocElem } = describeProperty(Document.prototype, 'documentElement');
-const { appendChild } = Document.prototype; // same as Node.appendChild
+const { document } = global;
+const { appendChild, getElementsByTagName } = document;
 const stringIncludes = String.prototype.includes;
+
+const elemByTag = tag => document::getElementsByTagName(tag)[0];
+
+let contLists;
+let pgLists;
+let realms;
 
 bridge.addHandlers({
   // FF bug workaround to enable processing of sourceURL in injected page scripts
-  InjectList(runAt) {
-    bridge.realms[INJECT_PAGE].lists[runAt]::forEach(inject);
-  },
+  InjectList: injectList,
 });
 
 export function appendToRoot(node) {
   // DOM spec allows any elements under documentElement
   // https://dom.spec.whatwg.org/#node-trees
-  const root = document::getHead() || document::getDocElem();
+  const root = elemByTag('head') || elemByTag('html');
   return root && root::appendChild(node);
 }
 
@@ -55,26 +53,26 @@ export function injectPageSandbox(contentId, webId) {
 export function injectScripts(contentId, webId, data, isXml) {
   bridge.ids = data.ids;
   // eslint-disable-next-line prefer-rest-params
-  if (!document::getDocElem()) return waitForDocElem(() => injectScripts(...arguments));
+  if (!elemByTag('html')) return onElement('html', injectScripts, ...arguments);
   let injectable = isXml ? false : null;
   const bornReady = ['interactive', 'complete'].includes(document.readyState);
-  const INFO = objectPick(data, ['cache', 'isFirefox', 'ua']);
-  const realms = {
+  const info = objectPick(data, ['cache', 'isFirefox', 'ua']);
+  realms = {
     [INJECT_CONTENT]: {
       injectable: () => true,
-      lists: { start: [], body: [], end: [], idle: [] },
+      lists: contLists = { start: [], body: [], end: [], idle: [] },
       ids: [],
-      info: INFO,
+      info,
     },
     [INJECT_PAGE]: {
       // eslint-disable-next-line no-return-assign
       injectable: () => injectable ?? (injectable = checkInjectable()),
-      lists: { start: [], body: [], end: [], idle: [] },
+      lists: pgLists = { start: [], body: [], end: [], idle: [] },
       ids: [],
-      info: INFO,
+      info,
     },
   };
-  const triage = (script) => {
+  sendCmd('InjectionFeedback', data.scripts.map((script) => {
     const { custom, dataKey, meta, props: { id } } = script;
     const desiredRealm = custom.injectInto || meta.injectInto || data.injectInto;
     const internalRealm = INJECT_MAPPING[desiredRealm] || INJECT_MAPPING[INJECT_AUTO];
@@ -86,31 +84,25 @@ export function injectScripts(contentId, webId, data, isXml) {
       let runAt = bornReady ? 'start'
         : `${custom.runAt || meta.runAt || ''}`.replace(/^document-/, '');
       const list = lists[runAt] || lists[runAt = 'end'];
-      script.action = realm === INJECT_CONTENT && (runAt === 'start' ? runAt : 'wait');
-      needsInjection = !!script.action;
-      ids::push(id);
-      list::push(script);
+      needsInjection = realm === INJECT_CONTENT;
+      script.stage = needsInjection && runAt !== 'start' && runAt;
+      ids.push(id);
+      list.push(script);
     } else {
       bridge.failedIds.push(id);
     }
     return [dataKey, needsInjection];
-  };
-  const feedback = data.scripts.map(triage);
-  setupContentInvoker(realms, contentId, webId);
-  sendCmd('InjectionFeedback', feedback);
-  injectAll(realms, 'start');
-  if (!bornReady && realms[INJECT_PAGE].ids.length) {
-    delete realms[INJECT_CONTENT];
-    if (realms[INJECT_PAGE].lists.body.length) {
-      onDocumentBody(() => injectAll(realms, 'body'));
-    }
-    document.addEventListener('DOMContentLoaded', async () => {
-      await 0;
-      injectAll(realms, 'end');
-      setTimeout(injectAll, 0, realms, 'idle');
+  }));
+  setupContentInvoker(contentId, webId);
+  injectAll('start');
+  if (pgLists.body[0] || contLists.body[0]) {
+    onElement('body', injectAll, 'body');
+  }
+  if (pgLists.idle[0] || contLists.idle[0] || pgLists.end[0] || contLists.end[0]) {
+    document::addEventListener('DOMContentLoaded', () => {
+      injectAll('end');
+      injectAll('idle');
     }, { once: true });
-  } else {
-    bridge.realms = null;
   }
 }
 
@@ -147,48 +139,64 @@ function inject(item) {
   script::remove();
 }
 
-function injectAll(realms, runAt) {
-  bridge.realms = realms;
-  realms::forEachEntry(([realm, realmData]) => {
+function injectAll(runAt) {
+  const isStart = runAt === 'start';
+  // Not using destructuring of arrays because we don't know if @@iterator is safe.
+  realms::forEachKey((realm) => {
+    const realmData = realms[realm];
     const isPage = realm === INJECT_PAGE;
-    realmData.lists::forEachEntry(([name, items]) => {
-      if ((!isPage || name === runAt) && items.length) {
-        bridge.post('ScriptData', { items, runAt, info: realmData.info }, realm);
+    realmData.lists::forEachKey((name) => {
+      const items = realmData.lists[name];
+      // All lists for content mode are processed jointly when runAt='start'
+      if ((isPage ? name === runAt : isStart) && items.length) {
+        bridge.post('ScriptData', { items, runAt: name, info: realmData.info }, realm);
         realmData.info = undefined;
-        items::forEach(item => {
-          // FF bug workaround to enable processing of sourceURL in injected page scripts
-          if (isPage && !bridge.isFirefox) inject(item);
-          item.code = '';
-        });
+        if (isPage && !bridge.isFirefox) {
+          injectList(runAt);
+        }
       }
     });
   });
+  if (!isStart && contLists[runAt].length) {
+    bridge.post('RunAt', runAt, INJECT_CONTENT);
+  }
   if (runAt === 'idle') {
-    bridge.realms = null;
+    realms = null;
+    pgLists = null;
+    contLists = null;
   }
 }
 
-function onDocumentBody(cb) {
-  if (document.body) {
-    cb();
+async function injectList(runAt) {
+  const isIdle = runAt === 'idle';
+  const list = pgLists[runAt];
+  // Not using for-of because we don't know if @@iterator is safe.
+  for (let i = 0; i < list.length; i += 1) {
+    if (isIdle) await sendCmd('SetTimeout', 0);
+    inject(list[i]);
+  }
+}
+
+function onElement(tag, cb, ...args) {
+  const elems = document::getElementsByTagName(tag);
+  if (elems[0]) {
+    cb(...args);
   } else {
     // This function runs before any userscripts, but MutationObserver callback may run
     // after content-mode userscripts so we'll have to use safe calls there
     const { disconnect } = MutationObserver.prototype;
-    const { get: getBody } = describeProperty(Document.prototype, 'body')
-      || describeProperty(HTMLDocument.prototype, 'body'); // old FF before 61;
-    const mo = new MutationObserver(() => {
-      if (document::getBody()) {
-        mo::disconnect();
-        cb();
+    const observer = new MutationObserver(() => {
+      if (elems[0]) {
+        observer::disconnect();
+        cb(...args);
       }
     });
-    // documentElement may be absent yet so we'll play it safe
-    mo.observe(document, { childList: true, subtree: true });
+    // documentElement may be replaced so we'll observe the entire document
+    observer.observe(document, { childList: true, subtree: true });
   }
 }
 
-function setupContentInvoker(realms, contentId, webId) {
+function setupContentInvoker(contentId, webId) {
   const invokableIds = realms[INJECT_CONTENT].ids;
   if (invokableIds.length) {
     const invoke = {
@@ -198,14 +206,4 @@ function setupContentInvoker(realms, contentId, webId) {
     bridge.invokableIds = invokableIds;
     bridge.post = (cmd, params, realm) => (invoke[realm] || postViaBridge)(cmd, params);
   }
-}
-
-function waitForDocElem(cb) {
-  const observer = new MutationObserver(() => {
-    if (document::getDocElem()) {
-      observer.disconnect();
-      cb();
-    }
-  });
-  observer.observe(document, { childList: true });
 }

src/background/index.js
@@ -1,4 +1,4 @@
-import { sendCmd } from '#/common';
+import { makePause, sendCmd } from '#/common';
 import { TIMEOUT_24HOURS, TIMEOUT_MAX } from '#/common/consts';
 import { deepCopy, forEachEntry, objectSet } from '#/common/object';
 import ua from '#/common/ua';
@@ -95,6 +95,14 @@ Object.assign(commands, {
         });
       }
     });
+  },
+  /**
+   * Timers in content scripts are shared with the web page so it can clear them.
+   * await sendCmd('SetTimeout', 100) in injected/content
+   * await bridge.send('SetTimeout', 100) in injected/web
+   */
+  SetTimeout(ms) {
+    return ms > 0 && makePause(ms);
   },
 });
 

src/injected/content/index.js
@@ -92,6 +92,7 @@ bridge.addHandlers({
     return styleId;
   },
   CheckScript: sendCmd,
+  SetTimeout: sendCmd,
 });
 
 function sendSetPopup() {

src/injected/web/bridge.js
@@ -6,7 +6,6 @@ const handlers = {};
 const callbacks = {};
 const bridge = {
   callbacks,
-  load: () => {},
   addHandlers(obj) {
     assign(handlers, obj);
   },

src/injected/web/gm-api.js
@@ -16,7 +16,7 @@ import {
 } from '../utils/helpers';
 
 const {
-  atob, setTimeout,
+  atob,
   Blob, Error, TextDecoder, Uint8Array,
   Array: { prototype: { findIndex, indexOf } },
   Document: { prototype: { getElementById } },
@@ -234,15 +234,14 @@ function getResource(context, name, isBlob) {
   }
 }
 
-function downloadBlob(res) {
+async function downloadBlob(res) {
   const { context: { name, onload }, response } = res;
   const url = createObjectURL(response);
   const a = document::createElementNS(NS_HTML, 'a');
   a::setAttribute('href', url);
   if (name) a::setAttribute('download', name);
   a::dispatchEvent(new MouseEvent('click'));
-  setTimeout(() => {
-    revokeObjectURL(url);
-    onload?.(res);
-  }, 3000);
+  await bridge.send('SetTimeout', 3000);
+  revokeObjectURL(url);
+  onload?.(res);
 }

src/injected/web/index.js
@@ -13,7 +13,12 @@ import './tabs';
 // Make sure to call safe::methods() in code that may run after userscripts
 
 const { document } = global;
+const { then } = Promise.prototype;
 const { get: getCurrentScript } = describeProperty(Document.prototype, 'currentScript');
+
+const sendSetTimeout = () => bridge.send('SetTimeout', 0);
+const resolvers = {};
+const waiters = {};
 
 export default function initialize(
   webId,
@@ -27,6 +32,9 @@ export default function initialize(
     invokeGuest = (cmd, data) => bridge.onHandle({ cmd, data });
     global.chrome = undefined;
     global.browser = undefined;
+    bridge.addHandlers({
+      RunAt: name => resolvers[name](),
+    });
   } else {
     bridge.mode = INJECT_PAGE;
     bridge.post = bindEvents(webId, contentId, bridge.onHandle);
@@ -36,11 +44,6 @@ export default function initialize(
       },
     });
   }
-  bridge.load = new Promise(resolve => {
-    // waiting for the page handlers to run first
-    bridge.loadResolve = async () => await 1 && resolve(1);
-    document.addEventListener('DOMContentLoaded', bridge.loadResolve, { once: true });
-  });
   return invokeGuest;
 }
 
@@ -58,6 +61,8 @@ bridge.addHandlers({
       store.cache = info.cache;
     }
     if (items) {
+      const { stage } = items[0];
+      if (stage) waiters[stage] = new Promise(resolve => { resolvers[stage] = resolve; });
       items::forEach(createScriptData);
       // FF bug workaround to enable processing of sourceURL in injected page scripts
       if (bridge.isFirefox && bridge.mode === INJECT_PAGE) {
@@ -82,8 +87,7 @@ bridge.addHandlers({
 function createScriptData(item) {
   const { dataKey, values } = item;
   store.values[item.props.id] = values;
-  if (window[dataKey]) {
-    // executeScript ran earlier than GetInjected response (improbable but theoretically possible)
+  if (window[dataKey]) { // executeScript ran before GetInjected response
     onCodeSet(item, window[dataKey]);
   } else {
     defineProperty(window, dataKey, {
@@ -94,15 +98,19 @@ function createScriptData(item) {
 }
 
 async function onCodeSet(item, fn) {
+  const { dataKey, stage } = item;
   // deleting now to prevent interception via DOMNodeRemoved on el::remove()
-  delete window[item.dataKey];
+  delete window[dataKey];
   if (process.env.DEBUG) {
     log('info', [bridge.mode], item.custom.name || item.meta.name || item.props.id);
   }
+  const run = () => wrapGM(item)::fn(logging.error);
   const el = document::getCurrentScript();
+  const wait = waiters[stage];
   if (el) el::remove();
-  if (item.action === 'wait') {
-    await bridge.load;
+  if (wait) {
+    waiters[stage] = (stage === 'idle' ? wait::then(sendSetTimeout) : wait)::then(run);
+  } else {
+    run();
   }
-  wrapGM(item)::fn(logging.error);
 }

commit 2708d0b44e244e6bd78642ebf12a2b61c7ce1ad2

fix: detect documentElement in XML pages
regressed in 136c1804

src/injected/content/inject.js
@@ -24,6 +24,7 @@ const { document } = global;
 const { appendChild, getElementsByTagName } = document;
 const stringIncludes = String.prototype.includes;
 
+/** When looking for documentElement, use '*' to also support XML pages */
 const elemByTag = tag => document::getElementsByTagName(tag)[0];
 
 let contLists;
@@ -38,7 +39,7 @@ bridge.addHandlers({
 export function appendToRoot(node) {
   // DOM spec allows any elements under documentElement
   // https://dom.spec.whatwg.org/#node-trees
-  const root = elemByTag('head') || elemByTag('html');
+  const root = elemByTag('head') || elemByTag('*');
   return root && root::appendChild(node);
 }
 
@@ -53,7 +54,7 @@ export function injectPageSandbox(contentId, webId) {
 export function injectScripts(contentId, webId, data, isXml) {
   bridge.ids = data.ids;
   // eslint-disable-next-line prefer-rest-params
-  if (!elemByTag('html')) return onElement('html', injectScripts, ...arguments);
+  if (!elemByTag('*')) return onElement('*', injectScripts, ...arguments);
   let injectable = isXml ? false : null;
   const bornReady = ['interactive', 'complete'].includes(document.readyState);
   const info = objectPick(data, ['cache', 'isFirefox', 'ua']);