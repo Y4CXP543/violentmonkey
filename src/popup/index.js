Y4CXP543/violentmonkey

commit bd58cba911adc365b1a5273ff4151e4bb9aa1f26

feat: faster load of popup, editor, dashboard

src/popup/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue';
-import { getActiveTab, i18n, sendCmd } from '#/common';
+import { getActiveTab, i18n, sendCmdDirectly } from '#/common';
 import { INJECT_PAGE, INJECTABLE_TAB_URL_RE } from '#/common/consts';
 import handlers from '#/common/handlers';
-import { mapEntry } from '#/common/object';
+import { loadScriptIcon } from '#/common/load-script-icon';
+import { forEachValue, mapEntry } from '#/common/object';
 import * as tld from '#/common/tld';
 import '#/common/ui/style';
 import App from './views/app';
 import { store } from './utils';
 
 tld.initTLD();
-
 Vue.prototype.i18n = i18n;
 
 const vm = new Vue({
@@ -30,7 +30,7 @@ mutex.ready = new Promise(resolve => {
 
 Object.assign(handlers, {
   async SetPopup(data, src) {
-    if (store.currentTab.id !== src.tab.id) return;
+    if (store.currentTab && store.currentTab.id !== src.tab.id) return;
     const isTop = src.frameId === 0;
     if (!isTop) await mutex.ready;
     const ids = data.ids.filter(id => !allScriptIds.includes(id));
@@ -42,7 +42,9 @@ Object.assign(handlers, {
     if (ids.length) {
       // frameScripts may be appended multiple times if iframes have unique scripts
       const scope = store[isTop ? 'scripts' : 'frameScripts'];
-      scope.push(...await sendCmd('GetMetas', ids));
+      const metas = data.metas || await sendCmdDirectly('GetMetas', ids);
+      metas.forEach(script => loadScriptIcon(script, { cache: store.cache }));
+      scope.push(...metas);
       data.failedIds.forEach(id => {
         scope.forEach((script) => {
           if (script.props.id === id) {
@@ -55,6 +57,10 @@ Object.assign(handlers, {
       });
     }
   },
+});
+
+sendCmdDirectly('CachePop', 'SetPopup').then((data) => {
+  data::forEachValue(val => handlers.SetPopup(...val));
 });
 
 getActiveTab()
@@ -70,6 +76,6 @@ getActiveTab()
   if (!INJECTABLE_TAB_URL_RE.test(url)) {
     store.injectable = false;
   } else {
-    store.blacklisted = await sendCmd('TestBlacklist', url);
+    store.blacklisted = await sendCmdDirectly('TestBlacklist', url);
   }
 });

src/background/index.js
@@ -1,6 +1,6 @@
 import { sendCmd } from '#/common';
 import { TIMEOUT_24HOURS, TIMEOUT_MAX } from '#/common/consts';
-import { forEachEntry, objectSet } from '#/common/object';
+import { deepCopy, forEachEntry, objectSet } from '#/common/object';
 import ua from '#/common/ua';
 import * as sync from './sync';
 import { commands } from './utils';
@@ -53,8 +53,8 @@ hookOptions((changes) => {
 
 Object.assign(commands, {
   /** @return {Promise<Object>} */
-  async GetData() {
-    const data = await getData();
+  async GetData(ids) {
+    const data = await getData(ids);
     data.sync = sync.getStates();
     return data;
   },
@@ -137,6 +137,8 @@ function autoUpdate() {
 }
 
 initialize(() => {
+  global.handleCommandMessage = handleCommandMessage;
+  global.deepCopy = deepCopy;
   browser.runtime.onMessage.addListener(
     ua.isFirefox // in FF a rejected Promise value is transferred only if it's an Error object
       ? (...args) => (

src/common/index.js
@@ -1,4 +1,5 @@
 import { browser } from '#/common/consts';
+import { deepCopy } from './object';
 import { noop } from './util';
 
 export { normalizeKeys } from './object';
@@ -37,6 +38,13 @@ export function initHooks() {
  */
 export function sendCmd(cmd, data, options) {
   return sendMessage({ cmd, data }, options);
+}
+
+export function sendCmdDirectly(cmd, data, options) {
+  const bg = browser.extension.getBackgroundPage?.();
+  return bg && bg !== window
+    ? bg.handleCommandMessage(bg.deepCopy({ cmd, data })).then(deepCopy)
+    : sendCmd(cmd, data, options);
 }
 
 /**
@@ -111,22 +119,6 @@ export function getFullUrl(url, base) {
 
 export function isRemote(url) {
   return url && !(/^(file:|data:|https?:\/\/localhost[:/]|http:\/\/127\.0\.0\.1[:/])/.test(url));
-}
-
-export function cache2blobUrl(raw, { defaultType, type: overrideType } = {}) {
-  if (raw) {
-    const parts = `${raw}`.split(',');
-    const { length } = parts;
-    const b64 = parts[length - 1];
-    const type = overrideType || parts[length - 2] || defaultType || '';
-    // Binary string is not supported by blob constructor,
-    // so we have to transform it into array buffer.
-    const bin = window.atob(b64);
-    const arr = new window.Uint8Array(bin.length);
-    for (let i = 0; i < bin.length; i += 1) arr[i] = bin.charCodeAt(i);
-    const blob = new Blob([arr], { type });
-    return URL.createObjectURL(blob);
-  }
 }
 
 export function encodeFilename(name) {

src/options/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
-import {
-  sendCmd, i18n, getLocaleString, cache2blobUrl,
-} from '#/common';
-import { forEachEntry, forEachValue } from '#/common/object';
+import { sendCmdDirectly, i18n, getLocaleString } from '#/common';
+import { forEachEntry } from '#/common/object';
 import handlers from '#/common/handlers';
 import options from '#/common/options';
 import loadZip from '#/common/zip';
@@ -51,27 +49,24 @@ function initScript(script) {
   script.$cache = { search, name, lowerName };
 }
 
-async function loadData() {
-  const data = await sendCmd('GetData', null, { retry: true });
-  if (!options.ready.indeed) await options.ready;
-  const oldCache = store.cache || {};
-  store.cache = data.cache;
-  store.sync = data.sync;
-  store.scripts = data.scripts;
-  if (store.scripts) {
-    store.scripts.forEach(initScript);
-  }
-  if (store.cache) {
-    store.cache::forEachEntry(([url, raw]) => {
-      if (oldCache[url]) {
-        store.cache[url] = oldCache[url];
-        delete oldCache[url];
-      } else {
-        store.cache[url] = cache2blobUrl(raw, { defaultType: 'image/png' });
-      }
+export async function loadData() {
+  const id = store.route.paths[1];
+  const params = id ? [+id].filter(Boolean) : null;
+  const [{ cache, scripts, sync }] = await Promise.all([
+    sendCmdDirectly('GetData', params, { retry: true }),
+    options.ready,
+  ]);
+  if (cache) {
+    const oldCache = store.cache || {};
+    cache::forEachEntry(([url, raw]) => {
+      const res = oldCache[url] || raw && `data:image/png;base64,${raw.split(',').pop()}`;
+      if (res) cache[url] = res;
     });
   }
-  oldCache::forEachValue(URL.revokeObjectURL);
+  scripts?.forEach(initScript);
+  store.scripts = scripts;
+  store.cache = cache;
+  store.sync = sync;
   store.loading = false;
 }
 

src/popup/utils/index.js
@@ -1,4 +1,5 @@
 export const store = {
+  cache: {},
   scripts: [],
   frameScripts: [],
   commands: [],

src/background/utils/cache.js
@@ -12,6 +12,9 @@ Object.assign(commands, {
   CacheHit(data) {
     cache.hit(data.key, data.lifetime);
   },
+  CachePop(key) {
+    return cache.pop(key) || null;
+  },
 });
 
 export default cache;

src/background/utils/db.js
@@ -331,9 +331,10 @@ function getIconUrls() {
  * @desc Get data for dashboard.
  * @return {Promise<{ scripts: VMScript[], cache: Object }>}
  */
-export async function getData() {
+export async function getData(ids) {
+  const { scripts } = store;
   return {
-    scripts: store.scripts,
+    scripts: ids ? ids.map(getScriptById) : scripts,
     cache: await storage.cache.getMulti(getIconUrls()),
   };
 }

src/background/utils/popup-tracker.js
@@ -1,10 +1,16 @@
-import { sendTabCmd } from '#/common';
+import { getActiveTab, sendTabCmd } from '#/common';
+import cache from './cache';
 import { postInitialize } from './init';
+import { commands } from './message';
 
 export const popupTabs = {}; // { tabId: 1 }
 
 postInitialize.push(() => {
   browser.runtime.onConnect.addListener(onPopupOpened);
+  browser.webRequest.onBeforeRequest.addListener(prefetchSetPopup, {
+    urls: [browser.runtime.getURL(browser.runtime.getManifest().browser_action.default_popup)],
+    types: ['main_frame'],
+  });
 });
 
 function onPopupOpened(port) {
@@ -12,9 +18,19 @@ function onPopupOpened(port) {
   popupTabs[tabId] = 1;
   sendTabCmd(tabId, 'PopupShown', true);
   port.onDisconnect.addListener(onPopupClosed);
+  delete commands.SetPopup;
 }
 
 function onPopupClosed({ name }) {
   delete popupTabs[name];
   sendTabCmd(+name, 'PopupShown', false);
+}
+
+async function prefetchSetPopup() {
+  const tabId = (await getActiveTab()).id;
+  sendTabCmd(tabId, 'PopupShown', true);
+  commands.SetPopup = async (data, src) => {
+    data.metas = commands.GetMetas(data.ids);
+    cache.put('SetPopup', Object.assign({ [src.frameId]: [data, src] }, cache.get('SetPopup')));
+  };
 }

commit 0f1448fc5f253a4c1d395788d60931162105c5f8

fix: show frame-only scripts correctly
regressed in bd58cba9

src/popup/index.js
@@ -42,7 +42,8 @@ Object.assign(handlers, {
     if (ids.length) {
       // frameScripts may be appended multiple times if iframes have unique scripts
       const scope = store[isTop ? 'scripts' : 'frameScripts'];
-      const metas = data.metas || await sendCmdDirectly('GetMetas', ids);
+      const metas = data.metas?.filter(({ props: { id } }) => ids.includes(id))
+        || await sendCmdDirectly('GetMetas', ids);
       metas.forEach(script => loadScriptIcon(script, { cache: store.cache }));
       scope.push(...metas);
       data.failedIds.forEach(id => {