Y4CXP543/violentmonkey

commit f167b62f0d34edd984bc838a2904e618606e334b

fix: run delayed scripts when envStart is empty
i.e. when there are no scripts at document-start or document-body

src/injected/content/inject.js
@@ -54,21 +54,19 @@ export function appendToRoot(node) {
   return root && root::appendChild(node);
 }
 
-export function injectPageSandbox(contentId, webId) {
+export function injectPageSandbox() {
   inject({
-    code: `(${VMInitInjection}())('${webId}','${contentId}')\n//# sourceURL=${
+    code: `(${VMInitInjection}())('${bridge.webId}','${bridge.contentId}')\n//# sourceURL=${
       browser.runtime.getURL('sandbox/injected-web.js')
     }`,
   });
 }
 
 /**
- * @param {string} contentId
- * @param {string} webId
  * @param {VMGetInjectedData} data
  * @param {boolean} isXml
  */
-export async function injectScripts(contentId, webId, data, isXml) {
+export async function injectScripts(data, isXml) {
   // eslint-disable-next-line prefer-rest-params
   if (!elemByTag('*')) return onElement('*', injectScripts, ...arguments);
   const { hasMore, info } = data;
@@ -108,8 +106,9 @@ export async function injectScripts(contentId, webId, data, isXml) {
   });
   // saving while safe
   const getReadyState = hasMore && describeProperty(Document.prototype, 'readyState').get;
-  if (realms[INJECT_CONTENT].is) {
-    setupContentInvoker(contentId, webId);
+  const hasInvoker = realms[INJECT_CONTENT].is;
+  if (hasInvoker) {
+    setupContentInvoker();
   }
   injectAll('start');
   const onBody = (pgLists.body[0] || contLists.body[0])
@@ -117,7 +116,7 @@ export async function injectScripts(contentId, webId, data, isXml) {
   // document-end, -idle
   if (hasMore) {
     data = await moreData;
-    if (data) await injectDelayedScripts(data, getReadyState);
+    if (data) await injectDelayedScripts(data, getReadyState, hasInvoker);
   }
   if (onBody) {
     await onBody;
@@ -127,16 +126,18 @@ export async function injectScripts(contentId, webId, data, isXml) {
   contLists = null;
 }
 
-async function injectDelayedScripts({ info, scripts }, getReadyState) {
+async function injectDelayedScripts({ info, scripts }, getReadyState, hasInvoker) {
   realms::forEachKey(r => {
-    realms[r].info = info;
+    assign(realms[r].info, info);
   });
+  let needsInvoker;
   scripts::forEach(script => {
     const { code, runAt } = script;
     if (code && !pageInjectable) {
       bridge.failedIds::push(script.props.id);
     } else {
       (code ? pgLists : contLists)[runAt]::push(script);
+      if (!code) needsInvoker = true;
     }
     script.stage = !code && runAt;
   });
@@ -144,6 +145,9 @@ async function injectDelayedScripts({ info, scripts }, getReadyState) {
     await new Promise(resolve => {
       document::addEventListener('DOMContentLoaded', resolve, { once: true });
     });
+  }
+  if (needsInvoker && !hasInvoker) {
+    setupContentInvoker();
   }
   injectAll('end');
   injectAll('idle');
@@ -239,8 +243,8 @@ function onElement(tag, cb, ...args) {
   });
 }
 
-function setupContentInvoker(contentId, webId) {
-  const invokeContent = VMInitInjection()(webId, contentId, bridge.onHandle);
+function setupContentInvoker() {
+  const invokeContent = VMInitInjection()(bridge.webId, bridge.contentId, bridge.onHandle);
   const postViaBridge = bridge.post;
   bridge.post = (cmd, params, realm) => (
     (realm === INJECT_CONTENT ? invokeContent : postViaBridge)(cmd, params)

src/injected/content/index.js
@@ -21,25 +21,27 @@ let pendingSetPopup;
 const { split } = '';
 
 (async () => {
-  const contentId = getUniqId();
-  const webId = getUniqId();
+  const eventIds = [
+    bridge.contentId = getUniqId(),
+    bridge.webId = getUniqId(),
+  ];
   // injecting right now before site scripts can mangle globals or intercept our contentId
   // except for XML documents as their appearance breaks, but first we're sending
   // a request for the data because injectPageSandbox takes ~5ms
   const dataPromise = sendCmd('GetInjected', null, { retry: true });
   const isXml = document instanceof XMLDocument;
-  if (!isXml) injectPageSandbox(contentId, webId);
+  if (!isXml) injectPageSandbox();
   // detecting if browser.contentScripts is usable, it was added in FF59 as well as composedPath
   const data = IS_FIREFOX && Event.prototype.composedPath
     ? await getDataFF(dataPromise)
     : await dataPromise;
   // 1) bridge.post may be overridden in injectScripts
   // 2) cloneInto is provided by Firefox in content scripts to expose data to the page
-  bridge.post = bindEvents(contentId, webId, bridge.onHandle, global.cloneInto);
+  bridge.post = bindEvents(...eventIds, bridge.onHandle, global.cloneInto);
   bridge.ids = data.ids;
   bridge.isFirefox = data.info.isFirefox;
   bridge.injectInto = data.injectInto;
-  if (data.scripts) injectScripts(contentId, webId, data, isXml);
+  if (data.scripts) injectScripts(data, isXml);
   if (data.expose) bridge.post('Expose');
   isPopupShown = data.isPopupShown;
   sendSetPopup();

commit 6d738442ddc0eb19c17c4717ee6b17bc27882570

fix: keep info until end, regressed in f167b62f

src/injected/content/inject.js
@@ -191,7 +191,6 @@ function injectAll(runAt) {
     const items = realmData.lists[runAt];
     if (items.length) {
       bridge.post('ScriptData', { items, runAt, info: realmData.info }, realm);
-      realmData.info = undefined;
       if (realm === INJECT_PAGE && !bridge.isFirefox) {
         injectList(runAt);
       }