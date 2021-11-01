Y4CXP543/violentmonkey

commit 5a78bdd1254b57482bea93bbd78183e32bf03c97

fix: guard main globals in a same-origin page context
* tentatively fixes #1400
* reimplements GM_addElement properly, #1335
+ fixes registerScriptDataFF, regressed in 7c332075

src/injected/content/bridge.js
@@ -1,5 +1,6 @@
-import { sendCmd } from '#/common';
+import { isPromise, sendCmd } from '#/common';
 import { INJECT_PAGE, browser } from '#/common/consts';
+import { CALLBACK_ID, createNullObj, getOwnProp } from '../util';
 
 const allow = createNullObj();
 /** @type {Object.<string, MessageFromGuestHandler>} */
@@ -14,7 +15,7 @@ const assignHandlers = (dest, src, force) => {
   }
 };
 const bridge = {
-  __proto__: null, // Object.create(null) may be spoofed
+  __proto__: null,
   ids: [], // all ids including the disabled ones for SetPopup
   runningIds: [],
   // userscripts running in the content script context are messaged via invokeGuest
@@ -35,19 +36,23 @@ const bridge = {
     (allow[cmd] || (allow[cmd] = createNullObj()))[dataKey] = true;
   },
   // realm is provided when called directly via invokeHost
-  async onHandle({ cmd, data, dataKey }, realm) {
+  async onHandle({ cmd, data, dataKey, node }, realm) {
     const handle = handlers[cmd];
     if (!handle || !allow[cmd]?.[dataKey]) {
-      throw new Error(`[Violentmonkey] Invalid command: "${cmd}" on ${global.location.host}`);
+      throw new ErrorSafe(`[Violentmonkey] Invalid command: "${cmd}" on ${global.location.host}`);
     }
-    const callbackId = data?.callbackId;
-    const payload = callbackId ? data.payload : data;
-    let res = handle === true ? sendCmd(cmd, payload) : handle(payload, realm || INJECT_PAGE);
-    if (res instanceof Promise) {
+    const callbackId = data && data::getOwnProp(CALLBACK_ID);
+    if (callbackId) {
+      data = data.data;
+    }
+    let res = handle === true
+      ? sendCmd(cmd, data)
+      : node::handle(data, realm || INJECT_PAGE);
+    if (res && isPromise(res)) {
       res = await res;
     }
-    if (callbackId && res !== undefined) {
-      bridge.post('Callback', { callbackId, payload: res }, realm);
+    if (callbackId) {
+      bridge.post('Callback', { id: callbackId, data: res }, realm);
     }
   },
 };

src/injected/web/bridge.js
@@ -1,37 +1,29 @@
 import { getUniqId } from '#/common';
+import { CALLBACK_ID, createNullObj } from '../util';
 
 const handlers = createNullObj();
 const callbacks = createNullObj();
+/**
+ * @property {UAInjected} ua
+ */
 const bridge = {
-  __proto__: null, // Object.create(null) may be spoofed
+  __proto__: null,
   cache: createNullObj(),
   callbacks,
   addHandlers(obj) {
     assign(handlers, obj);
   },
-  onHandle({ cmd, data }) {
+  onHandle({ cmd, data, node }) {
     const fn = handlers[cmd];
-    if (fn) fn(data);
+    if (fn) node::fn(data);
   },
-  send(cmd, data, context) {
-    return new Promise(resolve => {
-      postWithCallback(cmd, data, context, resolve);
+  send(cmd, data, context, node) {
+    return new PromiseSafe(resolve => {
+      const id = getUniqId();
+      callbacks[id] = resolve;
+      bridge.post(cmd, { [CALLBACK_ID]: id, data }, context, node);
     });
   },
-  sendSync(cmd, data, context) {
-    let res;
-    postWithCallback(cmd, data, context, payload => { res = payload; });
-    return res;
-  },
 };
-
-function postWithCallback(cmd, data, context, cb) {
-  const id = getUniqId();
-  callbacks[id] = (payload) => {
-    delete callbacks[id];
-    cb(payload);
-  };
-  bridge.post(cmd, { callbackId: id, payload: data }, context);
-}
 
 export default bridge;

.eslintrc.js
@@ -1,24 +1,28 @@
 const acorn = require('acorn');
-const unsafeEnvironment = [
-  'src/injected/**/*.js',
-];
-// some functions are used by `injected`
-const unsafeSharedEnvironment = [
+const FILES_INJECTED = [`src/injected/**/*.js`];
+const FILES_CONTENT = [`src/injected/content/**/*.js`];
+const FILES_WEB = [`src/injected/web/**/*.js`];
+  // some functions are used by `injected`
+const FILES_SHARED = [
   'src/common/browser.js',
   'src/common/consts.js',
   'src/common/index.js',
   'src/common/object.js',
   'src/common/util.js',
 ];
-const unsafeAndSharedEnvironment = [
-  ...unsafeEnvironment,
-  ...unsafeSharedEnvironment,
-];
-const commonGlobals = getGlobals('src/common/safe-globals.js');
-const injectedGlobals = {
-  ...commonGlobals,
-  ...getGlobals('src/injected/safe-injected-globals.js'),
+
+const GLOBALS_COMMON = getGlobals('src/common/safe-globals.js');
+const GLOBALS_INJECTED = getGlobals(`src/injected/safe-globals-injected.js`);
+const GLOBALS_CONTENT = {
+  ...getGlobals(`src/injected/content/safe-globals-content.js`),
+  ...GLOBALS_INJECTED,
 };
+const GLOBALS_WEB = {
+  ...getGlobals(`src/injected/web/safe-globals-web.js`),
+  ...GLOBALS_INJECTED,
+  IS_FIREFOX: false, // passed as a parameter to VMInitInjection in webpack.conf.js
+};
+
 module.exports = {
   root: true,
   extends: [
@@ -34,24 +38,30 @@ module.exports = {
     // `browser` is a local variable since we remove the global `chrome` and `browser` in injected*
     // to prevent exposing them to userscripts with `@inject-into content`
     files: ['*'],
-    excludedFiles: unsafeAndSharedEnvironment,
+    excludedFiles: [...FILES_INJECTED, ...FILES_SHARED],
     globals: {
       browser: false,
-      ...commonGlobals,
+      ...GLOBALS_COMMON,
     },
   }, {
-    files: unsafeSharedEnvironment,
-    globals: commonGlobals,
+    files: FILES_SHARED,
+    globals: GLOBALS_COMMON,
   }, {
-    files: unsafeEnvironment,
-    globals: injectedGlobals,
+    files: FILES_WEB,
+    globals: GLOBALS_WEB,
   }, {
-    files: unsafeAndSharedEnvironment,
+    files: FILES_CONTENT,
+    globals: GLOBALS_CONTENT,
+  }, {
+    files: FILES_INJECTED,
+    excludedFiles: [...FILES_CONTENT, ...FILES_WEB],
+    // intersection of globals in CONTENT and WEB
+    globals: Object.keys(GLOBALS_CONTENT).reduce((res, key) => (
+      Object.assign(res, key in GLOBALS_WEB && { [key]: false })
+    ), {}),
+  }, {
+    files: [...FILES_INJECTED, ...FILES_SHARED],
     rules: {
-      // Whitelisting our safe globals
-      'no-restricted-globals': ['error',
-        ...require('confusing-browser-globals').filter(x => injectedGlobals[x] == null),
-      ],
       /* Our .browserslistrc targets old browsers so the compiled code for {...objSpread} uses
          babel's polyfill that calls methods like `Object.assign` instead of our safe `assign`.
          Ideally, `eslint-plugin-compat` should be used but I couldn't make it work. */
@@ -60,13 +70,14 @@ module.exports = {
         message: 'Object spread adds a polyfill in injected* even if unused by it',
       }, {
         selector: 'OptionalCallExpression',
-        message: 'Optional call in an unsafe environment',
+        message: 'Optional call uses .call(), which may be spoofed/broken in an unsafe environment',
+        // TODO: write a Babel plugin to use safeCall for this.
       }, {
         selector: 'ArrayPattern',
-        message: 'Array destructuring in an unsafe environment',
+        message: 'Destructuring via Symbol.iterator may be spoofed/broken in an unsafe environment',
       }, {
-        selector: 'CallExpression > SpreadElement',
-        message: 'Array spreading in an unsafe environment',
+        selector: ':matches(ArrayExpression, CallExpression) > SpreadElement',
+        message: 'Spreading via Symbol.iterator may be spoofed/broken in an unsafe environment',
       }],
     },
   }, {
@@ -100,8 +111,11 @@ module.exports = {
 function getGlobals(fileName) {
   const text = require('fs').readFileSync(fileName, { encoding: 'utf8' });
   const res = {};
-  acorn.parse(text, { ecmaVersion: 2018, sourceType: 'module' }).body.forEach(body => {
-    (body.declaration || body).declarations.forEach(function processId({ id: { name, properties } }) {
+  const tree = acorn.parse(text, { ecmaVersion: 2018, sourceType: 'module' });
+  tree.body.forEach(body => {
+    const { declarations } = body.declaration || body;
+    if (!declarations) return;
+    declarations.forEach(function processId({ id: { left, properties, name = left && left.name } }) {
       if (name) {
         // const NAME = whatever
         res[name] = false;

scripts/sandbox-globals.html
@@ -4,7 +4,7 @@
 <script>
 const el = document.getElementById('results');
 el.value = [
-  function boundMethods() {
+  function boundMethods(isHeader) {
     const keys = [];
     /* global globalThis */
     for (const k in globalThis) {
@@ -28,25 +28,38 @@ el.value = [
         }
       }
     }
-    return keys.sort().map(k => `'${k}',`).join('\n');
+    return {
+      header: "const MAYBE = vmOwnFunc; // something that can't be imitated by the page\n",
+      keys: keys,
+      value: 'MAYBE',
+    };
   },
   function unforgeables() {
     return Object.entries(Object.getOwnPropertyDescriptors(window))
     .filter(([, v]) => !v.configurable)
-    .map(([k]) => `'${k}',`)
-    .sort()
-    .join('\n');
+    .map(([k]) => k);
   },
-  function readonlys() {
+  function readonlyKeys() {
     return Object.entries(Object.getOwnPropertyDescriptors(window))
     .filter(([k, v]) => k >= 'a' && k <= 'z'
       && k !== 'webkitStorageInfo' // deprecated
       && v.get && !v.set && v.configurable)
-    .map(([k]) => `'${k}',`)
-    .sort()
-    .join('\n');
+    .map(([k]) => k);
   },
-].map(fn => `// ${fn.name}\n${fn()}\n`).join('\n');
+].map(fn => {
+  let res = fn();
+  if (Array.isArray(res)) res = { keys: res };
+  return `${res.header || ''}const ${fn.name} = {
+  __proto__: null,
+${
+  res.keys.sort()
+  .map(k => `  ${k}: ${res.value || 1},`)
+  .join('\n')
+}
+}
+`;
+}).join('\n');
+
 el.focus();
 el.select();
 </script>

scripts/webpack.conf.js
@@ -10,7 +10,15 @@ const { ListBackgroundScriptsPlugin } = require('./manifest-helper');
 const projectConfig = require('./plaid.conf');
 const mergedConfig = shallowMerge(defaultOptions, projectConfig);
 
-const INIT_FUNC_NAME = 'VMInitInjection';
+// Avoiding collisions with globals of a content-mode userscript
+const INIT_FUNC_NAME = `Violentmonkey:${
+  Buffer.from(
+    new Uint32Array(2)
+    .map(() => Math.random() * (2 ** 32))
+    .buffer,
+  ).toString('base64')
+}`;
+const VAULT_ID = '__VAULT_ID__';
 // eslint-disable-next-line import/no-dynamic-require
 const VM_VER = require(`${defaultOptions.distDir}/manifest.json`).version;
 const WEBPACK_OPTS = {
@@ -29,6 +37,11 @@ const MIN_OPTS = {
   parallel: true,
   sourceMap: true,
   terserOptions: {
+    compress: {
+      // `terser` often inlines big one-time functions inside a small "hot" function
+      reduce_funcs: false,
+      reduce_vars: false,
+    },
     output: {
       ascii_only: true,
     },
@@ -58,7 +71,7 @@ const pickEnvs = (items) => {
   })));
 };
 
-const definitions = new webpack.DefinePlugin({
+const defsObj = {
   ...pickEnvs([
     { key: 'DEBUG', def: false },
     { key: 'VM_VER', val: VM_VER },
@@ -68,39 +81,47 @@ const definitions = new webpack.DefinePlugin({
     { key: 'SYNC_ONEDRIVE_CLIENT_SECRET' },
   ]),
   'process.env.INIT_FUNC_NAME': JSON.stringify(INIT_FUNC_NAME),
-});
+  'process.env.VAULT_ID': VAULT_ID,
+};
+const defsRe = new RegExp(`\\b(${Object.keys(defsObj).join('|').replace(/\./g, '\\.')})\\b`, 'g');
+const definitions = new webpack.DefinePlugin(defsObj);
 
 // avoid running webpack bootstrap in a potentially hacked environment
 // after documentElement was replaced which triggered reinjection of content scripts
 const skipReinjectionHeader = `if (window['${INIT_FUNC_NAME}'] !== 1)`;
 // {entryName: path}
 const entryGlobals = {
-  common: './src/common/safe-globals.js',
-  injected: './src/injected/safe-injected-globals.js',
+  common: [
+    './src/common/safe-globals.js',
+  ],
+  'injected/content': [
+    './src/injected/safe-globals-injected.js',
+    './src/injected/content/safe-globals-content.js',
+  ],
+  'injected/web': [
+    './src/injected/safe-globals-injected.js',
+    './src/injected/web/safe-globals-web.js',
+  ],
 };
 
 /**
  * Adds a watcher for files in entryGlobals to properly recompile the project on changes.
  */
 const addWrapper = (config, name, callback) => {
-  if (!callback) { callback = name; name = ''; }
-  const globals = Object.entries(entryGlobals).filter(([key]) => name === key || !name);
-  const dirs = globals.map(([key]) => key).join('|');
   config.module.rules.push({
-    test: new RegExp(`/(${dirs})/index\\.js$`.replace(/\//g, /[/\\]/.source)),
+    test: new RegExp(`/${name}/.*?\\.js$`.replace(/\//g, /[/\\]/.source)),
     use: [{
       loader: './scripts/fake-dep-loader.js',
-      options: {
-        files: globals.map(([, path]) => path),
-      },
+      options: { files: entryGlobals[name] },
     }],
   });
   const reader = () => (
-    globals.map(([, path]) => (
-      fs.readFileSync(path, { encoding: 'utf8' })
-      .replace(/export\s+(?=const\s)/g, '')
-    ))
-  ).join('\n');
+    entryGlobals[name]
+    .map(path => fs.readFileSync(path, { encoding: 'utf8' }))
+    .join('\n')
+    .replace(/export\s+(?=(const|let)\s)/g, '')
+    .replace(defsRe, s => defsObj[s])
+  );
   config.plugins.push(new WrapperWebpackPlugin(callback(reader)));
 };
 
@@ -148,21 +169,24 @@ module.exports = Promise.all([
         && Object.assign(p.options, { ignoreOrder: true })
       ));
     }
-    config.plugins.push(new ListBackgroundScriptsPlugin());
+    config.plugins.push(new ListBackgroundScriptsPlugin({
+      minify: false, // keeping readable
+    }));
   }),
 
   modify('injected', './src/injected', (config) => {
-    addWrapper(config, getGlobals => ({
+    addWrapper(config, 'injected/content', getGlobals => ({
       header: () => `${skipReinjectionHeader} { ${getGlobals()}`,
       footer: '}',
     }));
   }),
 
   modify('injected-web', './src/injected/web', (config) => {
+    // TODO: replace WebPack's Object.*, .call(), .apply() with safe calls
     config.output.libraryTarget = 'commonjs2';
-    addWrapper(config, getGlobals => ({
+    addWrapper(config, 'injected/web', getGlobals => ({
       header: () => `${skipReinjectionHeader}
-        window['${INIT_FUNC_NAME}'] = function () {
+        window['${INIT_FUNC_NAME}'] = function (${VAULT_ID}, IS_FIREFOX) {
           var module = { exports: {} };
           ${getGlobals()}`,
       footer: `

src/background/index.js
@@ -48,16 +48,15 @@ Object.assign(commands, {
       resetValueOpener(tabId);
       clearRequestsByTabId(tabId);
     }
-    const res = await getInjectedScripts(url, tabId, frameId);
-    const { feedback, valOpIds } = res._tmp;
-    res.isPopupShown = popupTabs[tabId];
+    const { feedback, inject, valOpIds } = await getInjectedScripts(url, tabId, frameId);
+    inject.isPopupShown = popupTabs[tabId];
     // Injecting known content scripts without waiting for InjectionFeedback message.
     // Running in a separate task because it may take a long time to serialize data.
     if (feedback.length) {
       setTimeout(commands.InjectionFeedback, 0, { feedback }, src);
     }
     addValueOpener(tabId, frameId, valOpIds);
-    return res;
+    return inject;
   },
   /** @return {Promise<Object>} */
   async GetTabDomain() {
@@ -133,10 +132,17 @@ initialize(() => {
   if (ua.isChrome) {
     // Using declarativeContent to run content scripts earlier than document_start
     const api = global.chrome.declarativeContent;
-    api.onPageChanged.getRules(['inject'], rules => {
-      if (rules.length) return;
+    api.onPageChanged.getRules(async ([rule]) => {
+      const id = rule?.id;
+      const newId = process.env.INIT_FUNC_NAME;
+      if (id === newId) {
+        return;
+      }
+      if (id) {
+        await browser.declarativeContent.onPageChanged.removeRules([id]);
+      }
       api.onPageChanged.addRules([{
-        id: 'inject',
+        id: newId,
         conditions: [
           new api.PageStateMatcher({
             pageUrl: { urlContains: '://' }, // essentially like <all_urls>

src/background/utils/preinject.js
@@ -21,7 +21,11 @@ const TIME_KEEP_DATA = 60e3; // 100ms should be enough but the tab may hang or g
 const cacheCode = initCache({ lifetime: TIME_KEEP_DATA });
 const cache = initCache({
   lifetime: TIME_KEEP_DATA,
-  onDispose: async promise => (await promise).rcsPromise?.unregister(),
+  onDispose: async promise => {
+    const data = await promise;
+    const rcs = await data?.rcsPromise;
+    rcs?.unregister();
+  },
 });
 const KEY_EXPOSE = 'expose';
 const KEY_INJECT_INTO = 'defaultInjectInto';
@@ -40,7 +44,10 @@ Object.assign(commands, {
   async InjectionFeedback({ feedId, feedback, pageInjectable }, src) {
     feedback.forEach(processFeedback, src);
     if (feedId) {
-      const env = await cache.pop(feedId);
+      // cache cleanup when getDataFF outruns GetInjected
+      cache.del(feedId.cacheKey);
+      // envDelayed
+      const env = await cache.pop(feedId.envKey);
       if (env) {
         const { scripts } = env;
         env.forceContent = !pageInjectable;
@@ -122,7 +129,8 @@ function onOptionChanged(changes) {
 
 /** @return {Promise<Object>} */
 export function getInjectedScripts(url, tabId, frameId) {
-  return cache.pop(getKey(url, !frameId)) || prepare(url, tabId, frameId, true);
+  const key = getKey(url, !frameId);
+  return cache.pop(key) || prepare(key, url, tabId, frameId, true);
 }
 
 function getKey(url, isTop) {
@@ -148,7 +156,7 @@ function onSendHeaders({ url, tabId, frameId }) {
     // GetInjected message will be sent soon by the content script
     // and it may easily happen while getScriptsByURL is still waiting for browser.storage
     // so we'll let GetInjected await this pending data by storing Promise in the cache
-    cache.put(key, prepare(url, tabId, frameId), TIME_AFTER_SEND);
+    cache.put(key, prepare(key, url, tabId, frameId), TIME_AFTER_SEND);
   }
 }
 
@@ -157,47 +165,50 @@ function onHeadersReceived(info) {
   cache.hit(getKey(info.url, !info.frameId), TIME_AFTER_RECEIVE);
 }
 
-function prepare(url, tabId, frameId, isLate) {
-  /** @namespace VMGetInjectedData */
+function prepare(key, url, tabId, frameId, isLate) {
   const res = {
-    expose: !frameId
-      && url.startsWith('https://')
-      && expose[url.split('/', 3)[2]],
+    /** @namespace VMGetInjectedData */
+    inject: {
+      expose: !frameId
+        && url.startsWith('https://')
+        && expose[url.split('/', 3)[2]],
+    },
   };
   return isApplied
-    ? prepareScripts(url, tabId, frameId, isLate, res)
+    ? prepareScripts(res, key, url, tabId, frameId, isLate)
     : res;
 }
 
-async function prepareScripts(url, tabId, frameId, isLate, res) {
+async function prepareScripts(res, cacheKey, url, tabId, frameId, isLate) {
   const data = await getScriptsByURL(url, !frameId);
   const { envDelayed, scripts } = data;
   const feedback = scripts.map(prepareScript, data).filter(Boolean);
   const more = envDelayed.promise;
-  const feedId = getUniqId(`${tabId}:${frameId}:`);
+  const envKey = getUniqId(`${tabId}:${frameId}:`);
+  const { inject } = res;
   /** @namespace VMGetInjectedData */
-  Object.assign(res, {
-    feedId, // InjectionFeedback id for envDelayed
+  Object.assign(inject, {
     injectInto,
     scripts,
+    feedId: {
+      cacheKey, // InjectionFeedback cache key for cleanup when getDataFF outruns GetInjected
+      envKey, // InjectionFeedback cache key for envDelayed
+    },
     hasMore: !!more, // tells content bridge to expect envDelayed
     ids: data.disabledIds, // content bridge adds the actually running ids and sends via SetPopup
     info: {
       cache: data.cache,
-      isFirefox: ua.isFirefox,
       ua,
     },
   });
-  Object.defineProperty(res, '_tmp', {
-    value: {
-      feedback,
-      valOpIds: [...data[ENV_VALUE_IDS], ...envDelayed[ENV_VALUE_IDS]],
-    },
+  Object.assign(res, {
+    feedback,
+    valOpIds: [...data[ENV_VALUE_IDS], ...envDelayed[ENV_VALUE_IDS]],
+    rcsPromise: !isLate && IS_FIREFOX
+      ? registerScriptDataFF(inject, url, !!frameId)
+      : null,
   });
-  if (!isLate && browser.contentScripts) {
-    registerScriptDataFF(data, res, url, !!frameId);
-  }
-  if (more) cache.put(feedId, more);
+  if (more) cache.put(envKey, more);
   return res;
 }
 
@@ -252,8 +263,9 @@ function replaceWithFullWidthForm(s) {
   return String.fromCharCode(s.charCodeAt(0) - 0x20 + 0xFF00);
 }
 
-const resolveDataCodeStr = `(${(data) => {
-  // not using `window` because this code can't reach its replacement set by guardGlobals
+const resolveDataCodeStr = `(${function _(data) {
+  /* `function` is required to compile `this`, and `this` is required because our safe-globals
+   * shadows `window` so its name is minified and hence inaccessible here */
   const { vmResolve } = this;
   if (vmResolve) {
     vmResolve(data);
@@ -264,9 +276,8 @@ const resolveDataCodeStr = `(${(data) => {
 }})`;
 
 // TODO: rework the whole thing to register scripts individually with real `matches`
-function registerScriptDataFF(data, inject, url, allFrames) {
-  data::forEachEntry(([key]) => delete data[key]); // releasing the contents for garbage collection
-  data.rcsPromise = browser.contentScripts.register({
+function registerScriptDataFF(inject, url, allFrames) {
+  return browser.contentScripts?.register({
     allFrames,
     js: [{
       code: `${resolveDataCodeStr}(${JSON.stringify(inject)})`,

src/common/browser.js
@@ -2,40 +2,45 @@
 // for DOM elements with 'id' attribute which is a standard feature, more info:
 // https://github.com/mozilla/webextension-polyfill/pull/153
 // https://html.spec.whatwg.org/multipage/window-object.html#named-access-on-the-window-object
-if (!global.browser?.runtime?.sendMessage) {
+if (!IS_FIREFOX && !global.browser?.runtime) {
   // region Chrome
-  const { chrome, Error, Promise, Proxy } = global;
+  const { chrome, Proxy: ProxySafe } = global;
+  const { apply, bind } = ProxySafe;
   const MESSAGE = 'message';
   const STACK = 'stack';
-  /** onXXX like onMessage */
-  const isApiEvent = key => key[0] === 'o' && key[1] === 'n';
+  const isSyncMethodName = key => key === 'addListener'
+    || key === 'removeListener'
+    || key === 'hasListener'
+    || key === 'hasListeners';
   /** API types or enums or literal constants */
   const isFunction = val => typeof val === 'function';
   const isObject = val => typeof val === 'object';
-  const proxifyValue = (target, key, groupName, src, metaVal) => {
+  const proxifyValue = (target, key, src, metaVal) => {
     const srcVal = src[key];
     if (srcVal === undefined) return;
     let res;
     if (isFunction(metaVal)) {
       res = metaVal(src, srcVal);
     } else if (isFunction(srcVal)) {
-      res = metaVal === 0 || isApiEvent(groupName)
+      res = metaVal === 0 || isSyncMethodName(key) || !src::hasOwnProperty(key)
         ? srcVal::bind(src)
         : wrapAsync(src, srcVal); // eslint-disable-line no-use-before-define
     } else if (isObject(srcVal) && metaVal !== 0) {
-      res = proxifyGroup(key, srcVal, metaVal); // eslint-disable-line no-use-before-define
+      res = proxifyGroup(srcVal, metaVal); // eslint-disable-line no-use-before-define
     } else {
       res = srcVal;
     }
     target[key] = res;
     return res;
   };
-  const proxifyGroup = (groupName, src, meta) => new Proxy({}, {
-    get: (target, key) => (
-      target[key]
-      ?? proxifyValue(target, key, groupName, src, meta?.[key])
-    ),
+  const proxifyGroup = (src, meta) => new ProxySafe({}, {
+    get: (group, key) => group[key] ?? proxifyValue(group, key, src, meta?.[key]),
   });
+  /**
+   * @param {Object} thisArg - original API group
+   * @param {function} func - original API function
+   * @param {WrapAsyncPreprocessorFunc} [preprocessorFunc] - modifies the API callback's response
+    */
   const wrapAsync = (thisArg, func, preprocessorFunc) => (
     (...args) => {
       let resolve;
@@ -43,12 +48,12 @@ if (!global.browser?.runtime?.sendMessage) {
       /* Using resolve/reject to call API in the scope of this function, not inside Promise,
          because an API validation exception is thrown synchronously both in Chrome and FF
          so the caller can use try/catch to detect it like we've been doing in icon.js */
-      const promise = new Promise((_resolve, _reject) => {
+      const promise = new PromiseSafe((_resolve, _reject) => {
         resolve = _resolve;
         reject = _reject;
       });
       // Make the error messages actually useful by capturing a real stack
-      const stackInfo = new Error(`callstack before invoking ${func.name || 'chrome API'}:`);
+      const stackInfo = new ErrorSafe(`callstack before invoking ${func.name || 'chrome API'}:`);
       // A single parameter `result` is fine because we don't use API that return more
       args[args.length] = result => {
         const runtimeErr = chrome.runtime.lastError;
@@ -78,7 +83,7 @@ if (!global.browser?.runtime?.sendMessage) {
   // Both result and error must be explicitly specified to avoid prototype eavesdropping
   const wrapError = err => process.env.DEBUG && console.warn(err) || [
     null,
-    err instanceof Error
+    err instanceof ErrorSafe
       ? [err[MESSAGE], err[STACK]]
       : [err, ''],
   ];
@@ -95,7 +100,7 @@ if (!global.browser?.runtime?.sendMessage) {
     if (process.env.DEBUG) console.info('receive', message);
     try {
       const result = listener(message, sender);
-      if (result instanceof Promise) {
+      if (result && result::objectToString() === '[object Promise]') {
         sendResponseAsync(result, sendResponse);
         return true;
       }
@@ -109,7 +114,7 @@ if (!global.browser?.runtime?.sendMessage) {
       sendResponse(wrapError(err));
     }
   };
-  /** @returns {?} error */
+  /** @type {WrapAsyncPreprocessorFunc} */
   const unwrapResponse = (resolve, response) => (
     !response && 'null response'
     || response[1] // error created in wrapError
@@ -122,7 +127,7 @@ if (!global.browser?.runtime?.sendMessage) {
    * 0 = non-async method or the entire group
    * function = transformer like (originalObj, originalFunc): function
    */
-  global.browser = proxifyGroup('', chrome, {
+  global.browser = proxifyGroup(chrome, {
     extension: 0, // we don't use its async methods
     i18n: 0, // we don't use its async methods
     runtime: {
@@ -142,7 +147,7 @@ if (!global.browser?.runtime?.sendMessage) {
     },
   });
   // endregion
-} else if (process.env.DEBUG && !global.chrome.app) {
+} else if (process.env.DEBUG && IS_FIREFOX) {
   // region Firefox
   /* eslint-disable no-restricted-syntax */// this is a debug-only section
   let counter = 0;
@@ -169,10 +174,17 @@ if (!global.browser?.runtime?.sendMessage) {
     const { frameId, tab, url } = sender;
     log('on', [msg, { frameId, tab, url }], id);
     const result = listener(msg, sender);
-    (typeof result?.then === 'function' ? result : Promise.resolve(result))
+    (typeof result?.then === 'function' ? result : PromiseSafe.resolve(result))
     .then(data => log('on', [data], id, true), console.warn);
     return result;
   });
   /* eslint-enable no-restricted-syntax */
   // endregion
 }
+
+/**
+ * @callback WrapAsyncPreprocessorFunc
+ * @param {function(any)} resolve - called on success
+ * @param {any} response - API callback's response
+ * @returns {?string[]} - [errorMessage, errorStack] array on error
+ */

commit 57fd1fc15b9c519ead3fab0e0ce7b55150e809c8

fix: bridge.send/call, regressed in 5a78bdd1

src/injected/content/bridge.js
@@ -41,7 +41,7 @@ const bridge = {
     if (!handle || !allow[cmd]?.[dataKey]) {
       throw new ErrorSafe(`[Violentmonkey] Invalid command: "${cmd}" on ${global.location.host}`);
     }
-    const callbackId = data && data::getOwnProp(CALLBACK_ID);
+    const callbackId = data && getOwnProp(data, CALLBACK_ID);
     if (callbackId) {
       data = data.data;
     }