Y4CXP543/violentmonkey

commit f5239b4e160702af84b330ea195276d0fd25325e

fix: survive on sites that abuse some known Symbols
* Symbol.isConcatSpreadable
* Symbol.replace
* also use setOwnProp/getOwnProp more

src/injected/web/util-web.js
@@ -1,6 +1,16 @@
 import { INJECT_CONTENT } from '../util';
 import bridge from './bridge';
 
+const isConcatSpreadableSym = SymbolSafe.isConcatSpreadable;
+
+export const safeConcat = (dest, ...arrays) => {
+  if (!dest[isConcatSpreadableSym]) {
+    setOwnProp(dest, isConcatSpreadableSym, true);
+    arrays::forEach(arr => setOwnProp(arr, isConcatSpreadableSym, true));
+  }
+  return concat::apply(dest, arrays);
+};
+
 // Reference: https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/JSON#Polyfill
 const escMap = {
   __proto__: null,
@@ -38,7 +48,7 @@ export const jsonDump = (value, stack) => {
     res = `${value}`;
     break;
   case 'string':
-    res = `"${value::replace(escRE, escFunc)}"`;
+    res = `"${escRE::regexpReplace(value, escFunc)}"`;
     break;
   case 'object':
     if (!stack) {
@@ -97,7 +107,7 @@ export const FastLookup = (hubs = createNullObj()) => {
     toArray: () => {
       const values = objectValues(hubs);
       values::forEach((val, i) => { values[i] = objectKeys(val); });
-      return concat::apply([], values);
+      return safeConcat([], values);
     },
   };
   function getHub(val, autoCreate) {

scripts/webpack-protect-bootstrap-plugin.js
@@ -2,7 +2,7 @@ const escapeStringRegexp = require('escape-string-regexp');
 
 /**
  * WARNING! The following globals must be correctly assigned using wrapper-webpack-plugin.
- * toStringTag = Symbol.toStringTag
+ * toStringTagSym = Symbol.toStringTag
  * defineProperty = Object.defineProperty
  * hasOwnProperty = Object.prototype.hasOwnProperty
  * safeCall = Function.prototype.call.bind(Function.prototype.call)
@@ -30,7 +30,7 @@ class WebpackProtectBootstrapPlugin {
       ]]));
       hooks.requireExtensions.tap(NAME, src => replace(src, [
         ["(typeof Symbol !== 'undefined' && Symbol.toStringTag)", '(true)'],
-        ['Symbol.toStringTag', 'toStringTag'],
+        ['Symbol.toStringTag', 'toStringTagSym'],
         [/Object\.(defineProperty\([^){\n]+{)/g, `$1${NULL_PROTO},`],
         ['Object.create(null)', NULL_OBJ],
         ['for(var key in value)', 'for(const key in value)'],

src/injected/content/safe-globals-content.js
@@ -37,7 +37,7 @@ export const {
 } = Object;
 export const { random: mathRandom } = Math;
 export const regexpTest = RegExp[PROTO].test;
-export const { toStringTag } = Symbol; // used by ProtectWebpackBootstrapPlugin
+export const { toStringTag: toStringTagSym } = Symbol; // used by ProtectWebpackBootstrapPlugin
 export const { decode: tdDecode } = TextDecoderSafe[PROTO];
 export const { stopImmediatePropagation } = Event[PROTO];
 export const { get: getHref } = describeProperty(HTMLAnchorElement[PROTO], 'href');

src/injected/safe-globals-injected.js
@@ -20,9 +20,12 @@ export const WINDOW_FOCUS = 'window.focus';
 export const NS_HTML = 'http://www.w3.org/1999/xhtml';
 export const CALLBACK_ID = '__CBID';
 
+export const getObjectTypeTag = val => val && val::objectToString()::slice(8, -1);
+
 export const isFunction = val => typeof val === 'function';
 export const isObject = val => val !== null && typeof val === 'object';
-export const isPromise = val => val && val::objectToString() === '[object Promise]';
+// TODO: maybe use `val[toStringTagSym]` when strict_min_version > 78
+export const isPromise = val => getObjectTypeTag(val) === 'Promise';
 export const isString = val => typeof val === 'string';
 
 export const getOwnProp = (obj, key) => (

src/injected/web/gm-api-wrapper.js
@@ -1,7 +1,7 @@
 import bridge from './bridge';
 import { makeGmApi } from './gm-api';
 import { makeGlobalWrapper } from './gm-global-wrapper';
-import { makeComponentUtils } from './util-web';
+import { makeComponentUtils, safeConcat } from './util-web';
 
 /** Name in Greasemonkey4 -> name in GM */
 const GM4_ALIAS = {
@@ -28,7 +28,7 @@ export function makeGmApiWrapper(script) {
   // Add GM functions
   // Reference: http://wiki.greasespot.net/Greasemonkey_Manual:API
   const { meta } = script;
-  const grant = meta.grant || [];
+  const grant = meta.grant;
   if (grant.length === 1 && grant[0] === 'none') {
     grant.length = 0;
   }
@@ -64,9 +64,7 @@ export function makeGmApiWrapper(script) {
   }
   if (!gmApi && grant.length) gmApi = makeGmApi();
   grant::forEach((name) => {
-    // Spoofed String index getters won't be called within length, length itself is unforgeable
-    const gm4name = name.length > 3 && name[2] === '.' && name[0] === 'G' && name[1] === 'M'
-      && name::slice(3);
+    const gm4name = name::slice(0, 3) === 'GM.' && name::slice(3);
     const fn = gmApi[gm4name ? `GM_${GM4_ALIAS[gm4name] || gm4name}` : name];
     if (fn) {
       if (gm4name) {
@@ -94,7 +92,7 @@ function makeGmInfo(script, resources) {
     case 'exclude': // -> excludes
     case 'include': // -> includes
       key += 's';
-      val = []::concat(val);
+      val = safeConcat([], val);
       break;
     default:
     }

src/injected/web/gm-api.js
@@ -195,15 +195,12 @@ function webAddElement(parent, tag, attrs, context) {
      but we keep it for compatibility with GM_addStyle in VM of 2017-2019
      https://github.com/violentmonkey/violentmonkey/issues/217
      as well as for GM_addElement in Tampermonkey. */
-  safeDefineProperty(el, 'then', {
-    configurable: true,
-    value(callback) {
-      // prevent infinite resolve loop
-      delete el.then;
-      callback(el);
-    },
-  });
-  return el;
+  return setOwnProp(el, 'then', async cb => (
+    // Preventing infinite resolve loop
+    delete el.then
+    // Native Promise ignores non-function
+    && (isFunction(cb) ? cb(el) : el)
+  ));
 }
 
 function getResource(context, name, isBlob) {

src/injected/web/gm-global-wrapper.js
@@ -1,11 +1,11 @@
 import { INJECT_CONTENT } from '../util';
 import bridge from './bridge';
-import { FastLookup } from './util-web';
+import { FastLookup, safeConcat } from './util-web';
 
 /** The index strings that look exactly like integers can't be forged
  * but for example '011' doesn't look like 11 so it's allowed */
 const isFrameIndex = key => key >= 0 && key <= 0xFFFF_FFFE && key === `${+key}`;
-
+const scopeSym = SymbolSafe.unscopables;
 const globalKeysSet = FastLookup();
 const globalKeys = (function makeGlobalKeys() {
   const kWrappedJSObject = 'wrappedJSObject';
@@ -164,7 +164,7 @@ export function makeGlobalWrapper(local) {
   /* Browsers may return [object Object] for Object.prototype.toString(window)
      on our `window` proxy so jQuery libs see it as a plain object and throw
      when trying to clone its recursive properties like `self` and `window`. */
-  safeDefineProperty(local, toStringTag, { get: () => 'Window' });
+  safeDefineProperty(local, toStringTagSym, { get: () => 'Window' });
   const wrapper = new ProxySafe(local, {
     __proto__: null,
     defineProperty(_, name, desc) {
@@ -245,12 +245,13 @@ function makeOwnKeys(local, globals) {
   const names = getOwnPropertyNames(local)::filter(notIncludedIn, globals);
   const symbols = getOwnPropertySymbols(local)::filter(notIncludedIn, globals);
   const frameIndexes = [];
-  for (let i = 0, s; (global[s = `${i}`] || 0)::objectToString() === '[object Window]'; i += 1) {
+  for (let i = 0, s; getObjectTypeTag(global[s = `${i}`]) === 'Window'; i += 1) {
     if (!(s in local)) {
       setOwnProp(frameIndexes, s, s);
     }
   }
-  return []::concat(
+  return safeConcat(
+    [],
     globals === globalKeysSet ? globalKeys : globals.toArray(),
     frameIndexes,
     names,

src/injected/web/requests.js
@@ -36,7 +36,7 @@ function parseData(req, msg) {
   case 'document':
     res = new DOMParserSafe()::parseFromString(res,
       // Cutting everything after , or ; and trimming whitespace
-      msg.contentType::replace(/[,;].*|\s+/g, '') || 'text/html');
+      /[,;].*|\s+/g::regexpReplace(msg.contentType, '') || 'text/html');
     break;
   default:
   }
@@ -70,11 +70,10 @@ function callback(req, msg) {
       },
     });
     if (headers != null) req.headers = headers;
-    // Spoofed String/Array index getters won't be called within length, length itself is unforgeable
-    if (text != null) req.text = text.length && text[0] === 'same' ? response : text;
-    data.context = opts.context;
-    data.responseHeaders = req.headers;
-    data.responseText = req.text;
+    if (text != null) req.text = getOwnProp(text, 0) === 'same' ? response : text;
+    setOwnProp(data, 'context', opts.context);
+    setOwnProp(data, 'responseHeaders', req.headers);
+    setOwnProp(data, 'responseText', req.text);
     cb(data);
   }
   if (msg.type === 'loadend') delete idMap[req.id];
@@ -144,7 +143,7 @@ function getResponseType({ responseType = '' }) {
  * and ReadableStream, which Chrome can't transfer to isolated world via CustomEvent.
  */
 async function encodeBody(body) {
-  const wasBlob = body::objectToString() === '[object Blob]';
+  const wasBlob = getObjectTypeTag(body) === 'Blob';
   const blob = wasBlob ? body : await new ResponseSafe(body)::safeResponseBlob();
   const reader = new FileReaderSafe();
   return new PromiseSafe(resolve => {

commit 49e30b05db1ac18722f2a4de156dbad59b87884f

fix: toArray, regressed in f5239b4e
fixes #1448

src/injected/web/util-web.js
@@ -76,7 +76,7 @@ export const FastLookup = (hubs = createNullObj()) => {
     toArray: () => {
       const values = objectValues(hubs);
       values::forEach((val, i) => { values[i] = objectKeys(val); });
-      return safeConcat([], values);
+      return safeConcat::apply([], values);
     },
   };
   function getHub(val, autoCreate) {