Y4CXP543/violentmonkey

commit 14feecfa71f87adf4fcd1227cac16c3ca49c1390

fix: avoid dupes when importing
* simplifies and consolidates handling - we can still check isNew if necessary
* allows re-entry of parseScript() when checking the imported script
* allows the find-usages command in IDE

src/options/index.js
@@ -111,19 +111,18 @@ function initMain() {
     UpdateSync(data) {
       store.sync = data;
     },
-    async AddScript({ update }) {
+    async UpdateScript({ update, where } = {}) {
+      if (!update) return;
       update.message = '';
-      await initScriptAndSize(update);
-      store.scripts.push(update);
-    },
-    async UpdateScript(data) {
-      if (!data) return;
-      const index = store.scripts.findIndex(item => item.props.id === data.where.id);
-      if (index >= 0) {
-        const updated = Object.assign({}, store.scripts[index], data.update);
-        if (updated.error && !data.update.error) updated.error = null;
-        await initScriptAndSize(updated);
-        Vue.set(store.scripts, index, updated);
+      const { scripts } = store;
+      const index = scripts.findIndex(item => item.props.id === where.id);
+      const updated = Object.assign({}, scripts[index], update);
+      if (updated.error && !update.error) updated.error = null;
+      await initScriptAndSize(updated);
+      if (index < 0) {
+        scripts.push(updated);
+      } else {
+        Vue.set(scripts, index, updated);
       }
     },
     RemoveScript(id) {

src/background/utils/db.js
@@ -1,9 +1,7 @@
 import {
   compareVersion, i18n, getFullUrl, getScriptName, isRemote, sendCmd, trueJoin,
 } from '#/common';
-import {
-  CMD_SCRIPT_ADD, CMD_SCRIPT_UPDATE, INJECT_PAGE, INJECT_AUTO, TIMEOUT_WEEK,
-} from '#/common/consts';
+import { INJECT_PAGE, INJECT_AUTO, TIMEOUT_WEEK } from '#/common/consts';
 import { forEachEntry, forEachKey, forEachValue } from '#/common/object';
 import storage from '#/common/storage';
 import pluginEvents from '../plugin/events';
@@ -502,7 +500,7 @@ export async function updateScriptInfo(id, data) {
   script.config = { ...script.config, ...data.config };
   script.custom = { ...script.custom, ...data.custom };
   await storage.script.dump(script);
-  return sendCmd(CMD_SCRIPT_UPDATE, { where: { id }, update: script });
+  return sendCmd('UpdateScript', { where: { id }, update: script });
 }
 
 /** @return {Promise<{ isNew?, update, where }>} */
@@ -514,7 +512,6 @@ export async function parseScript(src) {
       message: src.message == null ? i18n('msgUpdated') : src.message || '',
     },
   };
-  let cmd = CMD_SCRIPT_UPDATE;
   let script;
   const oldScript = await getScript({ id: src.id, meta });
   if (oldScript) {
@@ -522,7 +519,6 @@ export async function parseScript(src) {
     script = { ...oldScript };
   } else {
     ({ script } = newScript());
-    cmd = CMD_SCRIPT_ADD;
     result.isNew = true;
     result.update.message = i18n('msgInstalled');
   }
@@ -552,7 +548,7 @@ export async function parseScript(src) {
   fetchResources(script, src);
   Object.assign(result.update, script, src.update);
   result.where = { id: script.props.id };
-  sendCmd(cmd, result);
+  sendCmd('UpdateScript', result);
   pluginEvents.emit('scriptChanged', result);
   return result;
 }
@@ -595,7 +591,7 @@ export async function fetchResources(script, resourceCache, reqOptions) {
     const error = errors.map(formatHttpError)::trueJoin('\n');
     if (error) {
       const message = i18n('msgErrorFetchingResource');
-      sendCmd(CMD_SCRIPT_UPDATE, {
+      sendCmd('UpdateScript', {
         update: { error, message },
         where: { id: script.props.id },
       });

src/background/utils/update.js
@@ -1,5 +1,4 @@
 import { getScriptName, i18n, request, compareVersion, sendCmd, trueJoin } from '#/common';
-import { CMD_SCRIPT_UPDATE } from '#/common/consts';
 import { fetchResources, getScriptById, getScripts, parseScript } from './db';
 import { parseMeta } from './script';
 import { getOption, setOption } from './options';
@@ -115,7 +114,7 @@ async function downloadUpdate({ props: { id }, meta, custom }) {
       error: error ? `${i18n('genericError')} ${error.status}, ${error.url}` : null,
       // `null` is transferable in Chrome unlike `undefined`
     });
-    sendCmd(CMD_SCRIPT_UPDATE, result);
+    sendCmd('UpdateScript', result);
   }
 }
 

src/common/consts.js
@@ -14,9 +14,6 @@ export const INJECT_MAPPING = {
   [INJECT_CONTENT]: [INJECT_CONTENT],
 };
 
-export const CMD_SCRIPT_ADD = 'AddScript';
-export const CMD_SCRIPT_UPDATE = 'UpdateScript';
-
 // Allow metadata lines to start with WHITESPACE? '//' SPACE
 // Allow anything to follow the predefined text of the metaStart/End
 // The SPACE must be on the same line and specifically \x20 as \s would also match \r\n\t

commit 77e4d9094a08d24f54e39c35362868439549acb8

fix: restore update messages, broken by 14feecfa

src/options/index.js
@@ -113,13 +113,13 @@ function initMain() {
     },
     async UpdateScript({ update, where } = {}) {
       if (!update) return;
-      update.message = '';
       const { scripts } = store;
       const index = scripts.findIndex(item => item.props.id === where.id);
       const updated = Object.assign({}, scripts[index], update);
       if (updated.error && !update.error) updated.error = null;
       await initScriptAndSize(updated);
       if (index < 0) {
+        update.message = '';
         scripts.push(updated);
       } else {
         Vue.set(scripts, index, updated);