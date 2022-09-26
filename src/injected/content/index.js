Y4CXP543/violentmonkey

commit f7ac48c84d3910aac17485d985dd575f676f0679

fix: guard against hooking toString, toStringTag
* use safe toString for Number, URL, Location in string templates with sensitive data
* don't use Symbol.toStringTag and check FormData directly
* refactor: combine createNullObj and pickIntoThis

src/injected/content/index.js
@@ -33,7 +33,7 @@ async function init() {
       : await dataPromise
   );
   const { allowCmd } = bridge;
-  bridge::pickIntoThis(data, [
+  createNullObj(bridge, data, [
     'ids',
     'injectInto',
   ]);

src/injected/content/requests.js
@@ -19,11 +19,10 @@ let downloadChain = promiseResolve();
 // TODO: extract all prop names used across files into consts.js to ensure sameness
 bridge.addHandlers({
   async HttpRequest(msg, realm) {
-    requests[msg.id] = {
-      __proto__: null,
+    requests[msg.id] = createNullObj({
       realm,
       wantsBlob: msg.xhrType === 'blob',
-    }::pickIntoThis(msg, [
+    }, msg, [
       'eventsToNotify',
       'fileName',
     ]);
@@ -113,7 +112,7 @@ async function revokeBlobAfterTimeout(url) {
 
 /** ArrayBuffer/Blob in Chrome incognito is transferred in string chunks */
 function receiveAllChunks(req, msg) {
-  req::pickIntoThis(msg, ['dataSize', 'contentType']);
+  createNullObj(req, msg, ['dataSize', 'contentType']);
   req.arr = new SafeUint8Array(req.dataSize);
   processChunk(req, msg.data.response, 0);
   return !req.gotChunks

src/injected/safe-globals-injected.js
@@ -19,13 +19,9 @@ export const WINDOW_CLOSE = 'window.close';
 export const WINDOW_FOCUS = 'window.focus';
 export const NS_HTML = 'http://www.w3.org/1999/xhtml';
 export const CALLBACK_ID = '__CBID';
-
-export const getObjectTypeTag = val => {
-  // objectToString may call @@toStringTag getter which may throw
-  try {
-    return val && val::objectToString()::slice(8, -1);
-  } catch (e) { /* NOP */ }
-};
+/** These toString are used to avoid leaking data when converting into a string */
+const { toString: numberToString } = 0;
+const { toString: URLToString } = URL[PROTO];
 
 export const isFunction = val => typeof val === 'function';
 export const isObject = val => val !== null && typeof val === 'object';
@@ -59,8 +55,33 @@ export const setOwnProp = (obj, key, value, mutable = true) => (
 
 export const vmOwnFuncToString = () => '[Violentmonkey property]';
 
-/** Using __proto__ because Object.create(null) may be spoofed */
-export const createNullObj = () => ({ __proto__: null });
+/**
+ * Helps avoid interception via `Object.prototype`.
+ * @param {Object} [dst] - target object to clear the prototype or to pick into
+ * @param {Object} [src] - source object to pick from
+ * @param {string[]} [keys] - all keys will be picked otherwise
+ * @returns {Object} `dst` if it's already without prototype, a new object otherwise
+ */
+export const createNullObj = (dst, src, keys) => {
+  const empty = (!dst || dst.__proto__) && { __proto__: null }; // eslint-disable-line no-proto
+  if (!dst) {
+    dst = empty;
+  } else if (empty) {
+    dst = assign(empty, dst);
+  }
+  if (src) {
+    if (keys) {
+      keys::forEach(key => {
+        if (src::hasOwnProperty(key)) {
+          dst[key] = src[key];
+        }
+      });
+    } else {
+      assign(dst, src);
+    }
+  }
+  return dst;
+};
 
 // WARNING! `obj` must use __proto__:null
 export const ensureNestedProp = (obj, bucketId, key, defaultValue) => {
@@ -79,8 +100,8 @@ export const vmOwnFunc = (func, toString) => (
   setOwnProp(func, 'toString', toString || vmOwnFuncToString, false)
 );
 
-// Avoiding the need to safe-guard a bunch of methods so we use just one
-export const safeGetUniqId = (prefix = 'VM') => `${prefix}${mathRandom()}`;
+// Using just one random() to avoid many methods in vault just for this
+export const safeGetUniqId = (prefix = 'VM') => prefix + mathRandom()::numberToString(36);
 
 /** args is [tags?, ...rest] */
 export const log = (level, ...args) => {
@@ -91,30 +112,12 @@ export const log = (level, ...args) => {
 };
 
 /**
- * Picks into `this`
- * WARNING! `this` must use __proto__:null or already have own properties on the picked keys.
- * @param {Object} obj
- * @param {string[]} keys
- * @returns {Object} same object as `this`
- */
-export function pickIntoThis(obj, keys) {
-  if (obj) {
-    keys::forEach(key => {
-      if (obj::hasOwnProperty(key)) {
-        this[key] = obj[key];
-      }
-    });
-  }
-  return this;
-}
-
-/**
  * Object.defineProperty seems to be inherently broken: it reads inherited props from desc
  * (even though the purpose of this API is to define own props) and then complains when it finds
  * invalid props like an inherited setter when you only provide `{value}`.
  */
 export const safeDefineProperty = (obj, key, desc) => (
-  defineProperty(obj, key, assign(createNullObj(), desc))
+  defineProperty(obj, key, createNullObj(desc))
 );
 
 export const safePush = (arr, val) => (

src/injected/web/gm-api.js
@@ -102,7 +102,7 @@ export function makeGmApi() {
       } else if (arg1) {
         name = arg1.name;
         onload = arg1.onload;
-        opts::pickIntoThis(arg1, [
+        createNullObj(opts, arg1, [
           'url',
           'headers',
           'timeout',
@@ -147,12 +147,9 @@ export function makeGmApi() {
       return webAddElement(null, 'style', { textContent: css, id: safeGetUniqId('VMst') }, this);
     },
     GM_openInTab(url, options) {
-      return onTabCreate(
-        isObject(options)
-          ? assign(createNullObj(), options, { url })
-          : { active: !options, url },
-        this,
-      );
+      options = createNullObj(isObject(options) ? options : { active: !options });
+      options.url = url;
+      return onTabCreate(options, this);
     },
     GM_notification(text, title, image, onclick) {
       const options = isObject(text) ? text : {

src/injected/web/gm-global-wrapper.js
@@ -4,7 +4,7 @@ import { FastLookup, safeConcat } from './util-web';
 
 /** The index strings that look exactly like integers can't be forged
  * but for example '011' doesn't look like 11 so it's allowed */
-const isFrameIndex = key => key >= 0 && key <= 0xFFFF_FFFE && key === `${+key}`;
+const isFrameIndex = key => key >= 0 && key <= 0xFFFF_FFFE && key === (+key)::numberToString();
 const scopeSym = SafeSymbol.unscopables;
 const globalKeysSet = FastLookup();
 const globalKeys = (function makeGlobalKeys() {
@@ -143,7 +143,7 @@ for (const name in unforgeables) { /* proto is null */// eslint-disable-line gua
   );
   let fn;
   if (info) {
-    info = assign(createNullObj(), info);
+    info = createNullObj(info);
     // currently only `document` and `window`
     if ((fn = info.get)) info.get = fn::bind(thisObj);
     // currently only `location`
@@ -165,7 +165,7 @@ builtinGlobals = null; // eslint-disable-line no-global-assign
  */
 export function makeGlobalWrapper(local) {
   const events = createNullObj();
-  const readonlys = assign(createNullObj(), readonlyKeys);
+  const readonlys = createNullObj(readonlyKeys);
   let globals = globalKeysSet; // will be copied only if modified
   /* Browsers may return [object Object] for Object.prototype.toString(window)
      on our `window` proxy so jQuery libs see it as a plain object and throw
@@ -254,7 +254,11 @@ function makeOwnKeys(local, globals) {
   const names = getOwnPropertyNames(local)::filter(notIncludedIn, globals);
   const symbols = getOwnPropertySymbols(local)::filter(notIncludedIn, globals);
   const frameIndexes = [];
-  for (let i = 0, s; getObjectTypeTag(global[s = `${i}`]) === 'Window'; i += 1) {
+  // No way to verify a cross-origin window is a `Window`, so let's just trust the numbers
+  for (let i = 0, s, w, len = getOwnProp(window, 'length');
+    i < len && isObject(w = window[s = i::numberToString()]) && getOwnProp(w, 'window') === w;
+    i += 1
+  ) {
     if (!(s in local)) {
       setOwnProp(frameIndexes, s, s);
     }

src/injected/web/requests.js
@@ -12,7 +12,7 @@ bridge.addHandlers({
 export function onRequestCreate(opts, context, fileName) {
   if (!opts.url) throw new SafeError('Required parameter "url" is missing.');
   const scriptId = context.id;
-  const id = safeGetUniqId(`VMxhr${scriptId}`);
+  const id = safeGetUniqId('VMxhr');
   const req = {
     __proto__: null,
     id,
@@ -95,13 +95,12 @@ function callback(req, msg) {
 
 function start(req, context, fileName) {
   const { id, scriptId } = req;
-  const opts = assign(createNullObj(), req.opts);
+  const opts = createNullObj(req.opts);
   // withCredentials is for GM4 compatibility and used only if `anonymous` is not set,
   // it's true by default per the standard/historical behavior of gmxhr
   const { data, withCredentials = true, anonymous = !withCredentials } = opts;
   idMap[id] = req;
-  bridge.post('HttpRequest', {
-    __proto__: null,
+  bridge.post('HttpRequest', createNullObj({
     id,
     scriptId,
     anonymous,
@@ -111,11 +110,8 @@ function start(req, context, fileName) {
       || (opts.binary || !isObject(data)) && [`${data}`]
       // FF56+ can send any cloneable data directly, FF52-55 can't due to https://bugzil.la/1371246
       || IS_FIREFOX && bridge.ua.browserVersion >= 56 && [data]
-      /* Chrome can't directly transfer FormData to isolated world so we explode it,
-       * trusting its iterator is usable because the only reason for a site to break it
-       * is to fight a userscript, which it can do by breaking FormData constructor anyway */
-      // eslint-disable-next-line no-restricted-syntax
-      || (getObjectTypeTag(data) === 'FormData' ? [[...data], 'fd'] : [data, 'bin']),
+      || getFormData(data)
+      || [data, 'bin'],
     eventsToNotify: [
       'abort',
       'error',
@@ -127,7 +123,7 @@ function start(req, context, fileName) {
       'timeout',
     ]::filter(key => isFunction(getOwnProp(opts, `on${key}`))),
     xhrType: getResponseType(opts.responseType),
-  }::pickIntoThis(opts, [
+  }, opts, [
     'headers',
     'method',
     'overrideMimeType',
@@ -136,6 +132,17 @@ function start(req, context, fileName) {
     'url',
     'user',
   ]), context);
+}
+
+/** Chrome can't directly transfer FormData to isolated world so we explode it,
+ * trusting its iterator is usable because the only reason for a site to break it
+ * is to fight a userscript, which it can do by breaking FormData constructor anyway */
+function getFormData(data) {
+  try {
+    return [[...data::formDataEntries()], 'fd']; // eslint-disable-line no-restricted-syntax
+  } catch (e) {
+    /**/
+  }
 }
 
 function getResponseType(responseType = '') {

src/injected/web/util-web.js
@@ -67,7 +67,7 @@ export const FastLookup = (hubs = createNullObj()) => {
     clone() {
       const clone = createNullObj();
       for (const group in hubs) { /* proto is null */// eslint-disable-line guard-for-in
-        clone[group] = assign(createNullObj(), hubs[group]);
+        clone[group] = createNullObj(hubs[group]);
       }
       return FastLookup(clone);
     },

src/injected/content/gm-api-content.js
@@ -66,7 +66,7 @@ export async function sendSetPopup(isDelayed) {
       await setPopupThrottle;
       setPopupThrottle = null;
     }
-    sendCmd('SetPopup', { menus, __proto__: null }::pickIntoThis(bridge, [
+    sendCmd('SetPopup', createNullObj({ menus }, bridge, [
       'ids',
       'injectInto',
       'runningIds',

commit f4d212fef4748fcf454fa0e2f0b967f7401128c8

fix: regressions and ambiguities in f7ac48c8
* createNullObj to always create a new null object
* pickIntoNullObj to pick into an existing null obj
* add throwIfProtoPresent in debug mode

src/injected/content/index.js
@@ -33,7 +33,7 @@ async function init() {
       : await dataPromise
   );
   const { allowCmd } = bridge;
-  createNullObj(bridge, data, [
+  pickIntoNullObj(bridge, data, [
     'ids',
     'injectInto',
   ]);

src/injected/content/inject.js
@@ -291,6 +291,7 @@ function inject(item, iframeCb) {
 }
 
 function injectAll(runAt) {
+  if (process.env.DEBUG) throwIfProtoPresent(realms);
   for (const realm in realms) { /* proto is null */// eslint-disable-line guard-for-in
     const realmData = realms[realm];
     const items = realmData.lists[runAt];

src/injected/content/requests.js
@@ -112,7 +112,7 @@ async function revokeBlobAfterTimeout(url) {
 
 /** ArrayBuffer/Blob in Chrome incognito is transferred in string chunks */
 function receiveAllChunks(req, msg) {
-  createNullObj(req, msg, ['dataSize', 'contentType']);
+  pickIntoNullObj(req, msg, ['dataSize', 'contentType']);
   req.arr = new SafeUint8Array(req.dataSize);
   processChunk(req, msg.data.response, 0);
   return !req.gotChunks

src/injected/safe-globals-injected.js
@@ -23,6 +23,11 @@ export const CALLBACK_ID = '__CBID';
 const { toString: numberToString } = 0;
 const { toString: URLToString } = URL[PROTO];
 
+export const throwIfProtoPresent = process.env.DEBUG && (obj => {
+  if (!obj || obj.__proto__) { // eslint-disable-line no-proto
+    throw 'proto is not null';
+  }
+});
 export const isFunction = val => typeof val === 'function';
 export const isObject = val => val !== null && typeof val === 'object';
 export const isPromise = val => {
@@ -55,36 +60,37 @@ export const setOwnProp = (obj, key, value, mutable = true) => (
 
 export const vmOwnFuncToString = () => '[Violentmonkey property]';
 
-/**
- * Helps avoid interception via `Object.prototype`.
- * @param {Object} [dst] - target object to clear the prototype or to pick into
- * @param {Object} [src] - source object to pick from
- * @param {string[]} [keys] - all keys will be picked otherwise
- * @returns {Object} `dst` if it's already without prototype, a new object otherwise
- */
-export const createNullObj = (dst, src, keys) => {
-  const empty = (!dst || dst.__proto__) && { __proto__: null }; // eslint-disable-line no-proto
-  if (!dst) {
-    dst = empty;
-  } else if (empty) {
-    dst = assign(empty, dst);
-  }
+/** `dst` must have a null proto */
+export const pickIntoNullObj = (dst, src, keys) => {
+  if (process.env.DEBUG) throwIfProtoPresent(dst);
   if (src) {
-    if (keys) {
-      keys::forEach(key => {
-        if (src::hasOwnProperty(key)) {
-          dst[key] = src[key];
-        }
-      });
-    } else {
-      assign(dst, src);
-    }
+    keys::forEach(key => {
+      if (src::hasOwnProperty(key)) {
+        dst[key] = src[key];
+      }
+    });
   }
   return dst;
 };
 
+/**
+ * Helps avoid interception via `Object.prototype`.
+ * @param {Object} [base] - source object to copy as a null-proto object
+ * @param {Object} [src] - source object to pick from
+ * @param {string[]} [keys] - all keys will be picked otherwise
+ * @returns {Object} `base` if it's already without prototype, a new object otherwise
+ */
+export const createNullObj = (base, src, keys) => {
+  // eslint-disable-next-line no-proto
+  const res = { __proto__: null };
+  if (base || src && !keys) assign(res, base, src);
+  if (src && keys) pickIntoNullObj(res, src, keys);
+  return res;
+};
+
 // WARNING! `obj` must use __proto__:null
 export const ensureNestedProp = (obj, bucketId, key, defaultValue) => {
+  if (process.env.DEBUG) throwIfProtoPresent(obj);
   const bucket = obj[bucketId] || (
     obj[bucketId] = createNullObj()
   );

src/injected/web/gm-api.js
@@ -63,6 +63,7 @@ export function makeGmApi() {
     GM_removeValueChangeListener(listenerId) {
       const keyHooks = changeHooks[this.id];
       if (!keyHooks) return;
+      if (process.env.DEBUG) throwIfProtoPresent(keyHooks);
       for (const key in keyHooks) { /* proto is null */// eslint-disable-line guard-for-in
         const hooks = keyHooks[key];
         if (listenerId in hooks) {
@@ -102,7 +103,7 @@ export function makeGmApi() {
       } else if (arg1) {
         name = arg1.name;
         onload = arg1.onload;
-        createNullObj(opts, arg1, [
+        pickIntoNullObj(opts, arg1, [
           'url',
           'headers',
           'timeout',
@@ -124,7 +125,7 @@ export function makeGmApi() {
       return onRequestCreate(opts, this, name);
     },
     GM_xmlhttpRequest(opts) {
-      return onRequestCreate(opts, this);
+      return onRequestCreate(createNullObj(opts), this);
     },
     /**
      * Bypasses site's CSP for inline `style`, `link`, and `script`.

src/injected/web/gm-global-wrapper.js
@@ -135,6 +135,7 @@ const boundMethods = {
   webkitResolveLocalFileSystemURL: MAYBE,
 };
 
+if (process.env.DEBUG) throwIfProtoPresent(unforgeables);
 for (const name in unforgeables) { /* proto is null */// eslint-disable-line guard-for-in
   let thisObj;
   let info = (
@@ -241,6 +242,7 @@ export function makeGlobalWrapper(local) {
       delete desc.set;
       desc.value = wrapper;
     }
+    if (process.env.DEBUG) throwIfProtoPresent(desc);
     /* proto is already null */// eslint-disable-next-line no-restricted-syntax
     defineProperty(local, name, desc);
   }

src/injected/web/requests.js
@@ -9,8 +9,9 @@ bridge.addHandlers({
   },
 });
 
+/** `opts` must already have a null proto */
 export function onRequestCreate(opts, context, fileName) {
-  opts = createNullObj(opts);
+  if (process.env.DEBUG) throwIfProtoPresent(opts);
   let { url } = opts;
   if (url && !isString(url)) { // USVString in XMLHttpRequest spec calls ToString
     try {

src/injected/web/util-web.js
@@ -66,6 +66,7 @@ export const FastLookup = (hubs = createNullObj()) => {
     },
     clone() {
       const clone = createNullObj();
+      if (process.env.DEBUG) throwIfProtoPresent(clone);
       for (const group in hubs) { /* proto is null */// eslint-disable-line guard-for-in
         clone[group] = createNullObj(hubs[group]);
       }