Y4CXP543/violentmonkey

commit 0eee17dcaa4304d68dbe3d0082d99702832666f3

fix: move vulnerable web API usage to content mode

src/injected/content/inject.js
@@ -2,11 +2,8 @@ import { INJECT_CONTENT, INJECT_MAPPING, INJECT_PAGE, browser } from '#/common/c
 import { sendCmd } from '#/common';
 import { forEachKey } from '#/common/object';
 import bridge from './bridge';
-import { allowCommands, appendToRoot, onElement } from './util-content';
-import {
-  NS_HTML, bindEvents, fireBridgeEvent,
-  getUniqIdSafe, isSameOriginWindow, log,
-} from '../util';
+import { appendToRoot, onElement } from './util-content';
+import { bindEvents, fireBridgeEvent } from '../util';
 
 const INIT_FUNC_NAME = process.env.INIT_FUNC_NAME;
 const VAULT_SEED_NAME = INIT_FUNC_NAME + process.env.VAULT_ID_NAME;
@@ -111,16 +108,19 @@ export async function injectScripts(contentId, webId, data) {
       info,
     },
   };
+  assign(bridge.cache, data.cache);
   const feedback = data.scripts.map((script) => {
     const { id } = script.props;
     // eslint-disable-next-line no-restricted-syntax
     const realm = INJECT_MAPPING[script.injectInto].find(key => realms[key]?.injectable);
     // If the script wants this specific realm, which is unavailable, we won't inject it at all
     if (realm) {
+      const { pathMap } = script.custom;
       const realmData = realms[realm];
       realmData.lists[script.runAt].push(script); // 'start' or 'body' per getScriptsByURL()
       realmData.is = true;
-      allowCommands(script);
+      if (pathMap) bridge.pathMaps[id] = pathMap;
+      bridge.allowScript(script);
     } else {
       bridge.failedIds.push(id);
     }
@@ -158,9 +158,7 @@ export async function injectScripts(contentId, webId, data) {
 }
 
 async function injectDelayedScripts(contentId, webId, { cache, scripts }, getReadyState) {
-  realms::forEachKey(r => {
-    realms[r].info.cache = cache;
-  });
+  assign(bridge.cache, cache);
   let needsInvoker;
   scripts::forEach(script => {
     const { code, runAt } = script;
@@ -182,7 +180,7 @@ async function injectDelayedScripts(contentId, webId, { cache, scripts }, getRea
   if (needsInvoker && contentId) {
     setupContentInvoker(contentId, webId);
   }
-  scripts::forEach(allowCommands);
+  scripts::forEach(bridge.allowScript);
   injectAll('end');
   injectAll('idle');
 }
@@ -216,7 +214,6 @@ function injectAll(runAt) {
     const { info } = realmData;
     if (items.length) {
       bridge.post('ScriptData', { info, items, runAt }, realm);
-      info.cache = null;
       if (realm === INJECT_PAGE && !IS_FIREFOX) {
         injectList(runAt);
       }

.eslintignore
@@ -6,5 +6,6 @@
 !scripts/plaid.conf.js
 !scripts/webpack.conf.js
 !scripts/webpack-protect-bootstrap-plugin.js
+!scripts/webpack-util.js
 !gulpfile.js
 src/public/**

.eslintrc.js
@@ -1,6 +1,10 @@
-const acorn = require('acorn');
+const { readGlobalsFile } = require('./scripts/webpack-util');
+
 const FILES_INJECTED = [`src/injected/**/*.js`];
-const FILES_CONTENT = [`src/injected/content/**/*.js`];
+const FILES_CONTENT = [
+  'src/injected/*.js',
+  'src/injected/content/**/*.js',
+];
 const FILES_WEB = [`src/injected/web/**/*.js`];
   // some functions are used by `injected`
 const FILES_SHARED = [
@@ -108,14 +112,19 @@ module.exports = {
   },
 };
 
-function getGlobals(fileName) {
-  const text = require('fs').readFileSync(fileName, { encoding: 'utf8' });
+function getGlobals(filename) {
   const res = {};
-  const tree = acorn.parse(text, { ecmaVersion: 2018, sourceType: 'module' });
-  tree.body.forEach(body => {
+  const { ast } = readGlobalsFile(filename, { ast: true });
+  ast.program.body.forEach(body => {
     const { declarations } = body.declaration || body;
     if (!declarations) return;
-    declarations.forEach(function processId({ id: { left, properties, name = left && left.name } }) {
+    declarations.forEach(function processId({
+      id: {
+        left,
+        properties,
+        name = left && left.name,
+      },
+    }) {
       if (name) {
         // const NAME = whatever
         res[name] = false;

scripts/webpack-util.js
@@ -1,0 +1,69 @@
+const fs = require('fs');
+const babelCore = require('@babel/core');
+const WrapperWebpackPlugin = require('wrapper-webpack-plugin');
+
+// {entryName: path}
+const entryGlobals = {
+  common: [
+    './src/common/safe-globals.js',
+  ],
+  'injected/content': [
+    './src/injected/safe-globals-injected.js',
+    './src/injected/content/safe-globals-content.js',
+  ],
+  'injected/web': [
+    './src/injected/safe-globals-injected.js',
+    './src/injected/web/safe-globals-web.js',
+  ],
+};
+
+/**
+ * Adds a watcher for files in entryGlobals to properly recompile the project on changes.
+ */
+function addWrapperWithGlobals(name, config, defsObj, callback) {
+  config.module.rules.push({
+    test: new RegExp(`/${name}/.*?\\.js$`.replace(/\//g, /[/\\]/.source)),
+    use: [{
+      loader: './scripts/fake-dep-loader.js',
+      options: { files: entryGlobals[name] },
+    }],
+  });
+  const defsRe = new RegExp(`\\b(${
+    Object.keys(defsObj)
+    .join('|')
+    .replace(/\./g, '\\.')
+  })\\b`, 'g');
+  const reader = () => (
+    entryGlobals[name]
+    .map(path => readGlobalsFile(path))
+    .join('\n')
+    .replace(defsRe, s => defsObj[s])
+  );
+  config.plugins.push(new WrapperWebpackPlugin(callback(reader)));
+}
+
+function getUniqIdB64() {
+  return Buffer.from(
+    new Uint32Array(2)
+    .map(() => Math.random() * (2 ** 32))
+    .buffer,
+  ).toString('base64');
+}
+
+function readGlobalsFile(filename, babelOpts = {}) {
+  const { ast, code = !ast } = babelOpts;
+  const src = fs.readFileSync(filename, { encoding: 'utf8' })
+  .replace(/\bexport\s+(function\s+(\w+))/g, 'const $2 = $1')
+  .replace(/\bexport\s+(?=(const|let)\s)/g, '');
+  const res = babelCore.transformSync(src, {
+    ...babelOpts,
+    ast,
+    code,
+    filename,
+  });
+  return ast ? res : res.code;
+}
+
+exports.addWrapperWithGlobals = addWrapperWithGlobals;
+exports.getUniqIdB64 = getUniqIdB64;
+exports.readGlobalsFile = readGlobalsFile;

scripts/webpack.conf.js
@@ -1,24 +1,17 @@
 const { modifyWebpackConfig, shallowMerge, defaultOptions } = require('@gera2ld/plaid');
 const { isProd } = require('@gera2ld/plaid/util');
-const fs = require('fs');
 const webpack = require('webpack');
-const WrapperWebpackPlugin = require('wrapper-webpack-plugin');
 const HTMLInlineCSSWebpackPlugin = isProd && require('html-inline-css-webpack-plugin').default;
 const TerserPlugin = isProd && require('terser-webpack-plugin');
 const deepmerge = isProd && require('deepmerge');
 const { ListBackgroundScriptsPlugin } = require('./manifest-helper');
+const { addWrapperWithGlobals, getUniqIdB64 } = require('./webpack-util');
 const ProtectWebpackBootstrapPlugin = require('./webpack-protect-bootstrap-plugin');
 const projectConfig = require('./plaid.conf');
 const mergedConfig = shallowMerge(defaultOptions, projectConfig);
 
 // Avoiding collisions with globals of a content-mode userscript
-const INIT_FUNC_NAME = `Violentmonkey:${
-  Buffer.from(
-    new Uint32Array(2)
-    .map(() => Math.random() * (2 ** 32))
-    .buffer,
-  ).toString('base64')
-}`;
+const INIT_FUNC_NAME = `Violentmonkey:${getUniqIdB64()}`;
 const VAULT_ID = '__VAULT_ID__';
 const HANDSHAKE_ID = '__HANDSHAKE_ID__';
 // eslint-disable-next-line import/no-dynamic-require
@@ -88,47 +81,11 @@ const defsObj = {
   'process.env.HANDSHAKE_ID': HANDSHAKE_ID,
   'process.env.HANDSHAKE_ACK': '1',
 };
-const defsRe = new RegExp(`\\b(${Object.keys(defsObj).join('|').replace(/\./g, '\\.')})\\b`, 'g');
 const definitions = new webpack.DefinePlugin(defsObj);
 
 // avoid running webpack bootstrap in a potentially hacked environment
 // after documentElement was replaced which triggered reinjection of content scripts
 const skipReinjectionHeader = `if (window['${INIT_FUNC_NAME}'] !== 1)`;
-// {entryName: path}
-const entryGlobals = {
-  common: [
-    './src/common/safe-globals.js',
-  ],
-  'injected/content': [
-    './src/injected/safe-globals-injected.js',
-    './src/injected/content/safe-globals-content.js',
-  ],
-  'injected/web': [
-    './src/injected/safe-globals-injected.js',
-    './src/injected/web/safe-globals-web.js',
-  ],
-};
-
-/**
- * Adds a watcher for files in entryGlobals to properly recompile the project on changes.
- */
-const addWrapper = (config, name, callback) => {
-  config.module.rules.push({
-    test: new RegExp(`/${name}/.*?\\.js$`.replace(/\//g, /[/\\]/.source)),
-    use: [{
-      loader: './scripts/fake-dep-loader.js',
-      options: { files: entryGlobals[name] },
-    }],
-  });
-  const reader = () => (
-    entryGlobals[name]
-    .map(path => fs.readFileSync(path, { encoding: 'utf8' }))
-    .join('\n')
-    .replace(/export\s+(?=(const|let)\s)/g, '')
-    .replace(defsRe, s => defsObj[s])
-  );
-  config.plugins.push(new WrapperWebpackPlugin(callback(reader)));
-};
 
 const modify = (page, entry, init) => modifyWebpackConfig(
   (config) => {
@@ -154,7 +111,7 @@ const modify = (page, entry, init) => modifyWebpackConfig(
 
 module.exports = Promise.all([
   modify((config) => {
-    addWrapper(config, 'common', getGlobals => ({
+    addWrapperWithGlobals('common', config, defsObj, getGlobals => ({
       header: () => `{ ${getGlobals()}`,
       footer: '}',
       test: /^(?!injected|public).*\.js$/,
@@ -181,7 +138,7 @@ module.exports = Promise.all([
 
   modify('injected', './src/injected', (config) => {
     config.plugins.push(new ProtectWebpackBootstrapPlugin());
-    addWrapper(config, 'injected/content', getGlobals => ({
+    addWrapperWithGlobals('injected/content', config, defsObj, getGlobals => ({
       header: () => `${skipReinjectionHeader} { ${getGlobals()}`,
       footer: '}',
     }));
@@ -191,7 +148,7 @@ module.exports = Promise.all([
     // TODO: replace WebPack's Object.*, .call(), .apply() with safe calls
     config.output.libraryTarget = 'commonjs2';
     config.plugins.push(new ProtectWebpackBootstrapPlugin());
-    addWrapper(config, 'injected/web', getGlobals => ({
+    addWrapperWithGlobals('injected/web', config, defsObj, getGlobals => ({
       header: () => `${skipReinjectionHeader}
         window['${INIT_FUNC_NAME}'] = function (IS_FIREFOX,${HANDSHAKE_ID},${VAULT_ID}) {
           const module = { __proto__: null };

src/background/utils/db.js
@@ -279,7 +279,7 @@ export async function getScriptsByURL(url, isTop) {
   /** @namespace VMScriptByUrlData */
   const [envStart, envDelayed] = [0, 1].map(() => ({
     ids: [],
-    /** @type {VMInjectedScript[]} */
+    /** @type {(VMScript & VMInjectedScript)[]} */
     scripts: [],
     [ENV_CACHE_KEYS]: [],
     [ENV_REQ_KEYS]: [],

src/background/utils/preinject.js
@@ -190,6 +190,7 @@ async function prepareScripts(res, cacheKey, url, tabId, frameId, isLate) {
   Object.assign(inject, {
     injectInto,
     scripts,
+    cache: data.cache,
     feedId: {
       cacheKey, // InjectionFeedback cache key for cleanup when getDataFF outruns GetInjected
       envKey, // InjectionFeedback cache key for envDelayed
@@ -197,7 +198,6 @@ async function prepareScripts(res, cacheKey, url, tabId, frameId, isLate) {
     hasMore: !!more, // tells content bridge to expect envDelayed
     ids: data.disabledIds, // content bridge adds the actually running ids and sends via SetPopup
     info: {
-      cache: data.cache,
       ua,
     },
   });

src/background/utils/requests.js
@@ -29,7 +29,7 @@ Object.assign(commands, {
       xhr: new XMLHttpRequest(),
     };
     (tabRequests[tabId] || (tabRequests[tabId] = {}))[id] = 1;
-    httpRequest(opts, src, res => (
+    httpRequest(opts, src, res => requests[id] && (
       sendTabCmd(tabId, 'HttpRequested', res, { frameId })
     ));
   },
@@ -227,13 +227,12 @@ function xhrCallbackWrapper(req) {
   };
   return (evt) => {
     const type = evt.type;
-    if (type === 'loadend') clearRequest(req);
-    if (!req.cb) return;
     if (!contentType) {
       contentType = xhr.getResponseHeader('Content-Type') || 'application/octet-stream';
     }
     if (xhr.response !== response) {
       response = xhr.response;
+      sent = false;
       try {
         responseText = xhr.responseText;
         if (responseText === response) responseText = ['same'];
@@ -248,36 +247,44 @@ function xhrCallbackWrapper(req) {
     const shouldNotify = req.eventsToNotify.includes(type);
     // only send response when XHR is complete
     const shouldSendResponse = xhr.readyState === 4 && shouldNotify && !sent;
-    lastPromise = lastPromise
-    .then(() => shouldSendResponse && numChunks && getChunk(response, 0))
-    .then(chunk => req.cb({
-      blobbed,
-      chunked,
-      contentType,
-      dataSize,
-      id,
-      numChunks,
-      type,
-      data: shouldNotify && {
-        finalUrl: xhr.responseURL,
-        ...getResponseHeaders(),
-        ...objectPick(xhr, ['readyState', 'status', 'statusText']),
-        ...shouldSendResponse && {
-          response: chunk || response,
-          responseText,
+    lastPromise = lastPromise.then(async () => {
+      await req.cb({
+        blobbed,
+        chunked,
+        contentType,
+        dataSize,
+        id,
+        numChunks,
+        type,
+        data: shouldNotify && {
+          finalUrl: xhr.responseURL,
+          ...getResponseHeaders(),
+          ...objectPick(xhr, ['readyState', 'status', 'statusText']),
+          ...('loaded' in evt) && objectPick(evt, ['lengthComputable', 'loaded', 'total']),
+          response: shouldSendResponse
+            ? numChunks && await getChunk(response, 0) || response
+            : null,
+          responseText: shouldSendResponse
+            ? responseText
+            : null,
         },
-        ...('loaded' in evt) && objectPick(evt, ['lengthComputable', 'loaded', 'total']),
-      },
-    }));
-    if (shouldSendResponse) {
-      sent = true;
-      for (let i = 1; i < numChunks; i += 1) {
-        const last = i === numChunks - 1;
-        lastPromise = lastPromise
-        .then(() => getChunk(response, i)) // eslint-disable-line no-loop-func
-        .then(data => req.cb({ id, chunk: { data, i, last } }));
+      });
+      if (shouldSendResponse) {
+        for (let i = 1; i < numChunks; i += 1) {
+          await req.cb({
+            id,
+            chunk: {
+              i,
+              data: await getChunk(response, i),
+              last: i + 1 === numChunks,
+            },
+          });
+        }
       }
-    }
+      if (type === 'loadend') {
+        clearRequest(req);
+      }
+    });
   };
 }
 

commit 949805d59992e081a8f1f14207737c7ce3d21f95

fix: use pathMap in delayed scripts
fixes #1589
broken in 0eee17dc

src/injected/content/inject.js
@@ -205,8 +205,11 @@ async function injectDelayedScripts(contentId, webId, { cache, scripts }) {
   assign(bridge.cache, cache);
   let needsInvoker;
   scripts::forEach(script => {
-    const { code, runAt } = script;
+    const { code, runAt, custom: { pathMap } } = script;
     const { id } = script.props;
+    if (pathMap) {
+      bridge.pathMaps[id] = pathMap;
+    }
     if (!code) {
       needsInvoker = true;
       contLists[runAt]::push(script);