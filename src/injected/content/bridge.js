Y4CXP543/violentmonkey

commit bb59ed1c1b143c55e55959f01f06bd4a05e96bb1

feat: list scripts that failed to compile in popup

src/injected/content/bridge.js
@@ -16,12 +16,11 @@ const assignHandlers = (dest, src, force) => {
  */
 const bridge = {
   __proto__: null,
-  ids: [], // all ids including the disabled ones for SetPopup
-  runningIds: [],
-  // userscripts running in the content script context are messaged via invokeGuest
-  /** @type {Number[]} */
-  invokableIds: [],
-  failedIds: [],
+  /**
+   * -1 = bad realm, 0 = disabled, 1 = enabled, 2 = starting, 'page' | 'content' = running
+   * @type {{ [id: string]: -1 | 0 | 1 | 2 | 'page' | 'content' }}
+   */
+  ids: createNullObj(),
   cache: createNullObj(),
   pathMaps: createNullObj(),
   /** @type {function(VMInjection)[]} */

src/popup/index.js
@@ -1,6 +1,8 @@
 import '@/common/browser';
 import { sendCmdDirectly } from '@/common';
-import { INJECT_PAGE } from '@/common/consts';
+import {
+  ID_BAD_REALM, ID_INJECTING, INJECT_CONTENT, INJECT_INTO, INJECT_PAGE,
+} from '@/common/consts';
 import handlers from '@/common/handlers';
 import { loadScriptIcon } from '@/common/load-script-icon';
 import { forEachValue, mapEntry } from '@/common/object';
@@ -20,8 +22,9 @@ Object.assign(handlers, {
      * because we only show the iframe menu for unique scripts that don't run in the main page */
     const isTop = src.frameId === 0;
     if (!isTop) await mutex.ready;
-    const ids = data.ids.filter(id => !store.scriptIds.includes(id));
-    store.scriptIds.push(...ids);
+    const idMap = data.ids::mapEntry(null, (id, val) => store.idMap[id] !== val && id);
+    const ids = Object.keys(idMap).map(Number);
+    Object.assign(store.idMap, idMap);
     if (isTop) {
       mutex.resolve();
       store.commands = data.menus::mapEntry(Object.keys);
@@ -36,16 +39,18 @@ Object.assign(handlers, {
       metas.forEach(script => {
         loadScriptIcon(script, data);
         const { id } = script.props;
-        script.runs = data.runningIds.includes(id);
+        const state = idMap[id];
+        const badRealm = state === ID_BAD_REALM;
+        const renderedScript = scope.find(({ props }) => props.id === id);
+        if (renderedScript) script = renderedScript;
+        else scope.push(script);
+        script.runs = state === INJECT_CONTENT || state === INJECT_PAGE;
         script.pageUrl = src.url; // each frame has its own URL
-        if (data.failedIds.includes(id)) {
-          script.failed = true;
-          if (!store.injectionFailure) {
-            store.injectionFailure = { fixable: data.injectInto === INJECT_PAGE };
-          }
+        script.failed = badRealm || state === ID_INJECTING;
+        if (badRealm && !store.injectionFailure) {
+          store.injectionFailure = { fixable: data[INJECT_INTO] === INJECT_PAGE };
         }
       });
-      scope.push(...metas);
     }
   },
 });

src/types.d.ts
@@ -212,7 +212,7 @@ declare namespace VMInjection {
     disabledIds?: number[];
     /** Only present in envStart */
     envDelayed?: Env;
-    ids: number[];
+    ids: { [id: string]: NumBool };
     promise: Promise<Env>;
     reqKeys: string[];
     require: StringMap;

src/injected/content/index.js
@@ -6,10 +6,10 @@ import './notifications';
 import './requests';
 import './tabs';
 import { sendCmd } from './util';
-import { isEmpty, FORCE_CONTENT, INJECT_CONTENT } from '../util';
+import { isEmpty, FORCE_CONTENT, INJECT_CONTENT, INJECT_INTO } from '../util';
 import { Run } from './cmd-run';
 
-const { invokableIds } = bridge;
+const { ids } = bridge;
 
 // Make sure to call obj::method() in code that may run after INJECT_CONTENT userscripts
 async function init() {
@@ -33,10 +33,8 @@ async function init() {
       ? await getDataFF(dataPromise)
       : await dataPromise
   );
-  pickIntoNullObj(bridge, data, [
-    'ids',
-    'injectInto',
-  ]);
+  assign(ids, data.ids);
+  bridge[INJECT_INTO] = data[INJECT_INTO];
   if (data.expose && !isXml && injectPageSandbox(contentId, webId)) {
     bridge.addHandlers({ GetScriptVer: true }, true);
     bridge.post('Expose');
@@ -50,16 +48,13 @@ async function init() {
 }
 
 bridge.addBackgroundHandlers({
-  Command(data) {
-    const realm = invokableIds::includes(data.id) && INJECT_CONTENT;
-    bridge.post('Command', data, realm);
-  },
+  Command: data => bridge.post('Command', data, ids[data.id]),
   Run: id => Run(id, INJECT_CONTENT),
   UpdatedValues(data) {
     const dataPage = createNullObj();
     const dataContent = createNullObj();
     objectKeys(data)::forEach((id) => {
-      (invokableIds::includes(+id) ? dataContent : dataPage)[id] = data[id];
+      (ids[id] === INJECT_CONTENT ? dataContent : dataPage)[id] = data[id];
     });
     if (!isEmpty(dataPage)) bridge.post('UpdatedValues', dataPage);
     if (!isEmpty(dataContent)) bridge.post('UpdatedValues', dataContent, INJECT_CONTENT);

src/popup/utils/index.js
@@ -3,7 +3,7 @@ import { reactive } from 'vue';
 export const store = reactive({
   scripts: [],
   frameScripts: [],
-  scriptIds: [],
+  idMap: {},
   commands: [],
   domain: '',
   injectionFailure: null,

src/background/utils/db.js
@@ -257,7 +257,7 @@ export function getScriptsByURL(url, isTop, errors) {
  * @return {Promise<VMInjection.Env>}
  */
 async function getScriptEnv(scripts) {
-  const disabledIds = [];
+  const allIds = {};
   const [envStart, envDelayed] = [0, 1].map(() => ({
     depsMap: {},
     [ENV_SCRIPTS]: [],
@@ -268,8 +268,7 @@ async function getScriptEnv(scripts) {
   }
   scripts.forEach((script) => {
     const { id } = script.props;
-    if (!script.config.enabled) {
-      disabledIds.push(id);
+    if (!(allIds[id] = +!!script.config.enabled)) {
       return;
     }
     const { meta, custom } = script;
@@ -310,7 +309,7 @@ async function getScriptEnv(scripts) {
   if (envDelayed.ids.length) {
     envDelayed.promise = makePause().then(() => readEnvironmentData(envDelayed));
   }
-  return Object.assign(envStart, { disabledIds, envDelayed });
+  return Object.assign(envStart, { allIds, envDelayed });
 }
 
 async function readEnvironmentData(env) {

src/background/utils/popup-tracker.js
@@ -9,7 +9,7 @@ export const popupTabs = {}; // { tabId: 1 }
 addPublicCommands({
   async SetPopup(data, src) {
     if (popupTabs[src.tab.id]) return;
-    Object.assign(data, await getData({ ids: data.ids }));
+    Object.assign(data, await getData({ ids: Object.keys(data.ids) }));
     cache.put('SetPopup', Object.assign({ [src.frameId]: [data, src] }, cache.get('SetPopup')));
   },
 });

src/background/utils/preinject.js
@@ -1,6 +1,6 @@
 import { getScriptName, getScriptPrettyUrl, getUniqId, sendTabCmd } from '@/common';
 import {
-  INJECT_AUTO, INJECT_CONTENT, INJECT_MAPPING, INJECT_PAGE,
+  INJECT_AUTO, INJECT_CONTENT, INJECT_INTO, INJECT_MAPPING, INJECT_PAGE,
   FEEDBACK, FORCE_CONTENT, METABLOCK_RE, MORE, NEWLINE_END_RE,
 } from '@/common/consts';
 import initCache from '@/common/cache';
@@ -39,7 +39,6 @@ const cache = initCache({
   },
 });
 const INJECT = 'inject';
-const INJECT_INTO = 'injectInto';
 // KEY_XXX for hooked options
 const KEY_EXPOSE = 'expose';
 const KEY_DEF_INJECT_INTO = 'defaultInjectInto';
@@ -281,7 +280,7 @@ function prepare(key, url, tabId, frameId, forceContent) {
 async function prepareScripts(res, cacheKey, url, tabId, frameId, forceContent) {
   const errors = [];
   const bag = await getScriptsByURL(url, !frameId, errors);
-  const { envDelayed, disabledIds: ids, [ENV_SCRIPTS]: scripts } = bag;
+  const { envDelayed, allIds, [ENV_SCRIPTS]: scripts } = bag;
   bag[FORCE_CONTENT] = forceContent; // used in prepareScript and isPageRealm
   propsToClear::forEachValue(val => {
     if (val !== true) res[val] = bag[val];
@@ -301,11 +300,11 @@ async function prepareScripts(res, cacheKey, url, tabId, frameId, forceContent) 
     ),
     [MORE]: moreKey,
     cache: bag.cache,
-    ids, // content bridge adds the actually running ids and sends via SetPopup
+    ids: allIds,
     info: {
       ua,
     },
-    errors: errors.filter(err => !ids.includes(+err.slice(err.lastIndexOf('#') + 1))).join('\n'),
+    errors: errors.filter(err => allIds[err.split('#').pop()]).join('\n'),
   });
   res[FEEDBACK] = feedback;
   res[CSAPI_REG] = contentScriptsAPI && !xhrInject
@@ -462,7 +461,7 @@ function forceContentInjection(bag) {
   const inject = bag[INJECT];
   inject[FORCE_CONTENT] = true;
   inject[ENV_SCRIPTS].forEach(scr => {
-    // When script wants `page`, the result below will be `true` so the script goes into `failedIds`
+    // When script wants `page`, the result below will be `true` so the script has a "bad realm" id
     const failed = !isContentRealm(scr, true);
     scr.code = failed || '';
     bag[FEEDBACK].push([

commit 4224215080761d6f28bda4a7296d1ea356748f6c

fix: merge iframe-only scripts
regressed in bb59ed1c

src/injected/content/bridge.js
@@ -26,10 +26,7 @@ export const addBackgroundHandlers = addHandlersImpl.bind({}, bgHandlers);
  */
 const bridge = {
   __proto__: null,
-  /**
-   * -1 = bad realm, 0 = disabled, 1 = enabled, 2 = starting, 'page' | 'content' = running
-   * @type {{ [id: string]: -1 | 0 | 1 | 2 | 'page' | 'content' }}
-   */
+  /** @type {VMBridgeContentIds} */
   ids: createNullObj(),
   cache: createNullObj(),
   pathMaps: createNullObj(),

src/injected/web/bridge.js
@@ -9,6 +9,7 @@ const callbacks = {
 /**
  * @property {VMScriptGMInfoPlatform} ua
  * @property {VMBridgePostFunc} post
+ * @property {VMBridgeMode} mode
  */
 const bridge = {
   __proto__: null,

src/popup/index.js
@@ -22,7 +22,9 @@ Object.assign(handlers, {
      * because we only show the iframe menu for unique scripts that don't run in the main page */
     const isTop = src.frameId === 0;
     if (!isTop) await mutex.ready;
-    const idMap = data.ids::mapEntry(null, (id, val) => store.idMap[id] !== val && id);
+    const idMap = data.ids::mapEntry(null, (id, val, _) => (_ = store.idMap[id]) !== val
+      && (_ == null || isTop || val === ID_BAD_REALM || val === ID_INJECTING)
+      && id);
     const ids = Object.keys(idMap).map(Number);
     Object.assign(store.idMap, idMap);
     if (isTop) {

src/types.d.ts
@@ -99,6 +99,11 @@ declare namespace GMReq {
     }
   }
 }
+declare type VMBridgeMode = Exclude<VMScriptInjectInto, 'auto'>;
+declare type VMBridgeContentIds = {
+  /** -1 = bad realm, 0 = disabled, 1 = enabled, 2 = starting, context name = running */
+  [id: string]: -1 | 0 | 1 | 2 | VMBridgeMode;
+}
 declare type VMBridgePostFunc = (
   cmd: string,
   data: PlainJSONValue,