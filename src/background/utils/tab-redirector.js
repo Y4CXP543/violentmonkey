Y4CXP543/violentmonkey

commit a6f8e61c65cdb6bdd6b09ba93b18d3c5c29d4a7d

refactor: enable modern regex syntax via babel

src/background/utils/tab-redirector.js
@@ -37,23 +37,20 @@ Object.assign(commands, {
   },
 });
 
-const whitelistRe = new RegExp(`^https://(${
-  [
-    'greasyfork\\.org/scripts/%/code/',
-    'openuserjs\\.org/install/%/',
-    'github\\.com/%/%/raw/%/',
-    'github\\.com/%/%/releases/%/download/',
-    'raw\\.githubusercontent\\.com(/%){3}/',
-    'gist\\.github\\.com/.*?/',
-  ].join('|')
-})%?\\.user\\.js([?#]|$)`.replace(/%/g, '[^/]*'));
-const blacklistRe = new RegExp(`^https://(${
-  [
-    '(gist\\.)?github\\.com',
-    'greasyfork\\.org',
-    'openuserjs\\.org',
-  ].join('|')
-})/`);
+const whitelistRe = re`/^https:\/\/(
+  greasyfork\.org\/scripts\/[^/]*\/code|
+  openuserjs\.org\/install\/[^/]*|
+  github\.com\/[^/]*\/[^/]*\/(
+    raw\/[^/]*|
+    releases\/[^/]*\/download
+  )|
+  raw\.githubusercontent\.com(\/[^/]*){3}|
+  gist\.github\.com\/.*?
+)\/[^/]*?\.user\.js  ([?#]|$)  /ix`;
+const blacklistRe = re`/^https?:\/\/(
+  (gist\\.)?github\\.com|
+  (greasyfork|openuserjs)\\.org
+)\//ix`;
 const resolveVirtualUrl = url => (
   `${extensionRoot}options/index.html#scripts/${+url.split('#')[1]}`
 );

.babelrc.js
@@ -19,5 +19,6 @@ module.exports = {
   plugins: [
     './scripts/babel-plugin-safe-bind.js',
     ['@babel/plugin-transform-for-of', { assumeArray: true }],
+    ['transform-modern-regexp', { useRe: true }],
   ],
 };

.eslintrc.js
@@ -13,7 +13,10 @@ const FILES_SHARED = [
   'src/common/consts.js',
 ];
 
-const GLOBALS_COMMON = getGlobals('src/common/safe-globals.js');
+const GLOBALS_COMMON = {
+  ...getGlobals('src/common/safe-globals.js'),
+  re: false, // transform-modern-regexp with useRe option
+};
 const GLOBALS_INJECTED = getGlobals(`src/injected/safe-globals-injected.js`);
 const GLOBALS_CONTENT = {
   INIT_FUNC_NAME: false,

src/background/utils/requests-core.js
@@ -24,28 +24,34 @@ export const VM_VERIFY = getUniqId('VM-Verify');
 /** @type {Object<string,VMHttpRequest>} */
 export const requests = { __proto__: null };
 export const verify = { __proto__: null };
-export const FORBIDDEN_HEADER_RE = new RegExp(`^(proxy-|sec-|${[
-  'user-agent',
-  // https://developer.mozilla.org/en-US/docs/Glossary/Forbidden_header_name
-  // https://cs.chromium.org/?q=file:cc+symbol:IsForbiddenHeader%5Cb
-  'accept-(charset|encoding)',
-  'access-control-request-(headers|method)',
-  'connection',
-  'content-length',
-  'cookie2?',
-  'date',
-  'dnt',
-  'expect',
-  'host',
-  'keep-alive',
-  'origin',
-  'referer',
-  'te',
-  'trailer',
-  'transfer-encoding',
-  'upgrade',
-  'via',
-].join('|')})$`, 'i');
+export const FORBIDDEN_HEADER_RE = re`/
+^(
+  # prefix matches
+  proxy-|
+  sec-
+)|^(
+  # whole name matches
+  user-agent|
+  # https://developer.mozilla.org/en-US/docs/Glossary/Forbidden_header_name
+  # https://cs.chromium.org/?q=file:cc+symbol:IsForbiddenHeader%5Cb
+  accept-(charset|encoding)|
+  access-control-request-(headers|method)|
+  connection|
+  content-length|
+  cookie2?|
+  date|
+  dnt|
+  expect|
+  host|
+  keep-alive|
+  origin|
+  referer|
+  te|
+  trailer|
+  transfer-encoding|
+  upgrade|
+  via
+)$/ix`;
 /** @type chrome.webRequest.RequestFilter */
 const API_FILTER = {
   urls: ['<all_urls>'],
@@ -62,8 +68,10 @@ const isSendable = header => !isVmVerify(header)
   && !(/^origin$/i.test(header.name) && header.value === extensionOrigin);
 const isSendableAnon = header => isSendable(header) && isNotCookie(header);
 const SET_COOKIE_RE = /^set-cookie2?$/i;
-const SET_COOKIE_VALUE_RE = /^\s*(?:__(Secure|Host)-)?([^=\s]+)\s*=\s*(")?([!#-+\--:<-[\]-~]*)\3(.*)/;
-const SET_COOKIE_ATTR_RE = /\s*;?\s*(\w+)(?:=(")?([!#-+\--:<-[\]-~]*)\2)?/y;
+const SET_COOKIE_VALUE_RE = re`
+  /^\s*  (?:__(Secure|Host)-)?  ([^=\s]+)  \s*=\s*  (")?  ([!#-+\--:<-[\]-~]*)  \3(.*)  /x`;
+const SET_COOKIE_ATTR_RE = re`
+  /\s*  ;?\s*  (\w+)  (?:= (")?  ([!#-+\--:<-[\]-~]*)  \2)?  /xy`;
 const SAME_SITE_MAP = {
   strict: 'strict',
   lax: 'lax',

src/common/util.js
@@ -263,9 +263,22 @@ const FORCED_ACCEPT = {
   'greasyfork.org': 'application/javascript, text/plain, text/css',
 };
 
-export const isDataUri = url => /^data:/.test(url);
-export const isRemote = url => url
-  && !(/^(file:\/\/|data:|https?:\/\/([^@/]*@)?(localhost|127\.0\.0\.1|(192\.168|172\.16|10\.0)\.[0-9]+\.[0-9]+|\[(::1|(fe80|fc00)::[.:0-9a-f]+)\]|.+\.(test|example|invalid|localhost))(:[0-9]+|\/|$))/i.test(url));
+const isLocalUrlRe = re`/^(
+  file:\/\/|
+  data:|
+  https?:\/\/
+    ([^@/]*@)?
+    (
+      localhost|
+      127\.0\.0\.1|
+      (192\.168|172\.16|10\.0)\.\d+\.\d+|
+      \[(::1|(fe80|fc00)::[.:0-9a-f]+)]|
+      [^/:]+\.(test|example|invalid|localhost)
+    )
+    (:\d+|\/|$)
+)/ix`;
+export const isDataUri = url => /^data:/i.test(url);
+export const isRemote = url => url && !isLocalUrlRe.test(decodeURI(url));
 
 /** @typedef {{
   url: string,

commit 0caa72a280df00085cf9924baca5082d47d8a9bd

fix #1979: fix installer for github urls with ?raw=1
broken in a6f8e61

src/background/utils/tab-redirector.js
@@ -51,8 +51,8 @@ const whitelistRe = re`/^https:\/\/(
   gist\.github\.com\/.*?
 )\/[^/]*?\.user\.js  ([?#]|$)  /ix`;
 const blacklistRe = re`/^https?:\/\/(
-  (gist\\.)?github\\.com|
-  ((greas|sleaz)yfork|openuserjs)\\.org
+  (gist\.)?github\.com|
+  ((greas|sleaz)yfork|openuserjs)\.org
 )\//ix`;
 const resolveVirtualUrl = url => (
   `${extensionOptionsPage}${ROUTE_SCRIPTS}/${+url.split('#')[1]}`